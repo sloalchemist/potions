6c847f0d4f5ef4b69569a9473f658092dd7cd824
jhuanny <jhuan208@calpoly.edu>
2025-01-09 00:34:52 -0800

    fix: test case item/mob generation started

--- packages/server/test/items/uses/building.test.ts
+++ packages/server/test/items/uses/building.test.ts
@@ -1,7 +1,10 @@
-//import { ItemGenerator } from '../../../src/items/itemGenerator';
+import { ItemGenerator } from '../../../src/items/itemGenerator';
 import { commonSetup } from '../../testSetup';
 import { DB } from '../../../src/services/database';
-//import { Item } from '../../../src/items/item';
+import { Item } from '../../../src/items/item';
+import { BuildWall } from '../../../src/items/uses/building/buildWall';
+import { MobI } from '@rt-potion/common';
+import { Mob } from '../../../src/mobs/mob';
 
 beforeAll(() => {
   commonSetup();
@@ -9,15 +12,96 @@ beforeAll(() => {
 
 describe('Build wall from partial wall', () => {
   test('should build wall from partial wall', () => {
+    const worldDescription = {
+      tiles: [
+        [-1, -1],
+        [-1, -1]
+      ],
+      terrain_types: [],
+      item_types: [
+        {
+          name: 'Log',
+          description: 'A piece of wood',
+          type: 'log',
+          carryable: true,
+          smashable: true,
+          walkable: true,
+          interactions: [
+            {
+              "description": "Build wall",
+              "action": "build_wall",
+              "while_carried": true,
+              "requires_item": "partial-wall"
+          }
+          ],
+          attributes: [],
+          on_tick: []
+        },
+        {
+          name: 'Partial Wall',
+          description: 'A partially formed wall',
+          type: 'partial-wall',
+          carryable: false,
+          walkable: false,
+          interactions: [],
+          attributes: [
+            {
+              "name": "complete",
+              "value": 3
+            }
+          ]
+        }
+      ],
+      mob_types: [
+        {
+          name: 'Villager',
+          type: 'villager',
+          speaker: true
+        }
+      ]
+    };
+
     //generate world
+    const itemGenerator = new ItemGenerator(worldDescription.item_types);
+    const logPos = { x: 0, y: 0};
+    itemGenerator.createItem({
+      type: 'log',
+      position: logPos
+    });
 
-    //create partial wall
+    const wallPos = { x: 3, y: 3};
+    itemGenerator.createItem({
+      type: 'partial-wall',
+      position: wallPos
+    });
+    const logId = Item.getItemIDAt(logPos);
+    expect(logId).not.toBeNull();
+    const log = Item.getItem(logId!);
 
+    let villager: MobI;
+    villager = {
+      id: 'villager',
+      position: { x: 5, y: 5 },
+      type: 'villager',
+      subtype: '',
+      path: [],
+      speed: 1,
+      name: 'TestVillager',
+      maxHealth: 10,
+      carrying: undefined, 
+      attributes: {},
+      unlocks: [],
+      doing: ''
+    };
+    villager.carrying = logId;
+    const buildWall = new BuildWall();
+    const wallInteract = buildWall.interact(villager, log);
     //need to trigger "interact" in buildWall.ts
-
+    //mob.carrying = log?
     //expect there to be a wall at our coords
 
     //without bug fix there is a partial wall so test fails
+    //expect to be wall instead of partial wall 
     
   });
 });
