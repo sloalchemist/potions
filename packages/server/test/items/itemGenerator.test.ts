26e56af325aca7a621b2534ef2fbadfba2261c21
Jonathan DeBarro <jdebarro@calpoly.edu>
2025-01-10 13:48:44 -0800

    Added test to itemgenerator

--- packages/server/test/items/itemGenerator.test.ts
+++ packages/server/test/items/itemGenerator.test.ts
@@ -42,6 +42,40 @@ describe('Generate heart-beet', () => {
     expect(heartBeet).not.toBeNull();
     expect(heartBeet!.type).toBe('heart-beet');
   });
+    
+  test('Check that non walkable items cannot be walked to', () => {
+    const worldDescription = {
+        tiles: [
+          [-1, -1],
+          [-1, -1]
+        ],
+        terrain_types: [],
+        item_types: [
+          {
+            name: 'Solid Object',
+            description: 'test',
+            type: 'solid object',
+            carryable: true,
+            walkable: false, // This is what matters
+            interactions: [],
+            attributes: [],
+            on_tick: []
+          }
+        ],
+        mob_types: []
+    };
+
+    // Item generation is tested elsewhere
+    const itemGenerator = new ItemGenerator(worldDescription.item_types);
+    const position = { x: 1, y: 0 };
+    itemGenerator.createItem({
+        type: 'solid object',
+        position
+    });
+    const id = Item.getItemIDAt(position);
+    const solid_object = Item.getItem(id!);
+    expect(solid_object?.itemType.walkable).toBeFalsy();
+    });
 });
 
 afterAll(() => {
