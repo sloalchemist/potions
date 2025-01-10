21b34bd53b5d182f98ea9e37235b669c04bbb6f4
Xavier Garcia <xmangarcia17@gmail.com>
2025-01-10 14:25:10 -0800

    fix some errors with mobNegativeHealth.test.ts; need to identify root cause of foreign key issue

--- packages/server/test/mobs/mobNegativeHealth.test.ts
+++ packages/server/test/mobs/mobNegativeHealth.test.ts
@@ -1,7 +1,7 @@
 import { commonSetup } from '../testSetup';
-import { DB } from '../../src/services/database';
 import { mobFactory } from '../../src/mobs/mobFactory';
 import { Mob } from '../../src/mobs/mob';
+import { DB } from '../../src/services/database';
 
 beforeAll(() => {
   commonSetup();
@@ -10,36 +10,48 @@ beforeAll(() => {
 describe('Create mob and remove more health than it has', () => {
   test('should (1) create player mob, (2) remove all health, ' +
     '(3) health should be zero, not negative', () => {
-    // const worldDescription = {
-    //   tiles: [
-    //     [-1, -1],
-    //     [-1, -1]
-    //   ],
-    //   terrain_types: [],
-    //   item_types: [
-    //     {
-    //       name: 'Heart Beet',
-    //       description: 'A heart-shaped beet',
-    //       type: 'heart-beet',
-    //       carryable: true,
-    //       walkable: true,
-    //       interactions: [],
-    //       attributes: [],
-    //       on_tick: []
-    //     }
-    //   ],
-    //   mob_types: []
-    // };
+    const worldDescription = {
+      tiles: [
+        [-1, -1],
+        [-1, -1]
+      ],
+      terrain_types: [],
+      item_types: [],
+      mob_types: [
+        {
+            "name": "Player",
+            "description": "The player",
+            "name_style": "norse-english",
+            "type": "player",
+            "health": 100,
+            "speed": 2.5,
+            "attack": 5,
+            "gold": 0,
+            "community": "alchemists",
+            "stubbornness": 20,
+            "bravery": 5,
+            "aggression": 5,
+            "industriousness": 40,
+            "adventurousness": 10,
+            "gluttony": 50,
+            "sleepy": 80,
+            "extroversion": 50,
+            "speaker": true
+          }
+      ]
+    };
     const position = { x: 0, y: 0 };
+
+    // create mobFactory's mobTemplates
+    mobFactory.mobTemplates['player'] = worldDescription.mob_types[0];
     // create player mob
     mobFactory.makeMob(
         "player",
         position,
-        "test ID"
+        "1"
     );
-
     // query mob from world
-    const testMob = Mob.getMob("test ID");
+    const testMob = Mob.getMob("1");
     // check mob's initial health
     expect(testMob?.health).toBe(100);
     // change health of mob
