1b29e336666bca3e600f80e1a6798e00deda9bae
Xavier Garcia <xmangarcia17@gmail.com>
2025-01-10 11:25:06 -0800

    create test for checking mob for negative health

--- packages/server/test/mobs/mobNegativeHealth.test.ts
+++ packages/server/test/mobs/mobNegativeHealth.test.ts
@@ -1,45 +1,51 @@
-import { ItemGenerator } from '../../src/items/itemGenerator';
 import { commonSetup } from '../testSetup';
 import { DB } from '../../src/services/database';
-import { MobFactory } from '../../src/mobs/mobFactory';
+import { mobFactory } from '../../src/mobs/mobFactory';
+import { Mob } from '../../src/mobs/mob';
 
 beforeAll(() => {
   commonSetup();
 });
 
 describe('Create mob and remove more health than it has', () => {
-  test('should generate heart-beet', () => {
-    const worldDescription = {
-      tiles: [
-        [-1, -1],
-        [-1, -1]
-      ],
-      terrain_types: [],
-      item_types: [
-        {
-          name: 'Heart Beet',
-          description: 'A heart-shaped beet',
-          type: 'heart-beet',
-          carryable: true,
-          walkable: true,
-          interactions: [],
-          attributes: [],
-          on_tick: []
-        }
-      ],
-      mob_types: []
-    };
-    const mobGenerator = new MobFactory(worldDescription.item_types);
+  test('should (1) create player mob, (2) remove all health, ' +
+    '(3) health should be zero, not negative', () => {
+    // const worldDescription = {
+    //   tiles: [
+    //     [-1, -1],
+    //     [-1, -1]
+    //   ],
+    //   terrain_types: [],
+    //   item_types: [
+    //     {
+    //       name: 'Heart Beet',
+    //       description: 'A heart-shaped beet',
+    //       type: 'heart-beet',
+    //       carryable: true,
+    //       walkable: true,
+    //       interactions: [],
+    //       attributes: [],
+    //       on_tick: []
+    //     }
+    //   ],
+    //   mob_types: []
+    // };
     const position = { x: 0, y: 0 };
-    itemGenerator.createItem({
-      type: 'heart-beet',
-      position
-    });
-    const heartBeetID = Item.getItemIDAt(position);
-    expect(heartBeetID).not.toBeNull();
-    const heartBeet = Item.getItem(heartBeetID!);
-    expect(heartBeet).not.toBeNull();
-    expect(heartBeet!.type).toBe('heart-beet');
+    // create player mob
+    mobFactory.makeMob(
+        "player",
+        position,
+        "test ID"
+    );
+
+    // query mob from world
+    const testMob = Mob.getMob("test ID");
+    // check mob's initial health
+    expect(testMob?.health).toBe(100);
+    // change health of mob
+    testMob?.changeHealth(-110);
+    // check mob's new health
+    expect(testMob?.health).toBe(0);
   });
 });
 
