c02f0358940ec9b6f1de952121e565779c6f494e
Jonathan DeBarro <jdebarro@calpoly.edu>
2025-01-10 14:57:00 -0800

    Passed database name in common setup for test suites

--- packages/server/test/items/itemGenerator.test.ts
+++ packages/server/test/items/itemGenerator.test.ts
@@ -8,40 +8,40 @@ beforeAll(() => {
 });
 
 describe('Generate heart-beet', () => {
-  test('should generate heart-beet', () => {
-    const worldDescription = {
-      tiles: [
-        [-1, -1],
-        [-1, -1]
-      ],
-      terrain_types: [],
-      item_types: [
-        {
-          name: 'Heart Beet',
-          description: 'A heart-shaped beet',
-          type: 'heart-beet',
-          carryable: true,
-          walkable: true,
-          interactions: [],
-          attributes: [],
-          on_tick: []
-        }
-      ],
-      mob_types: []
-    };
-    const itemGenerator = new ItemGenerator(worldDescription.item_types);
-    //const world = new ServerWorld(worldDescription);
-    const position = { x: 0, y: 0 };
-    itemGenerator.createItem({
-      type: 'heart-beet',
-      position
+    test('should generate heart-beet', () => {
+        const worldDescription = {
+            tiles: [
+                [-1, -1],
+                [-1, -1]
+            ],
+            terrain_types: [],
+            item_types: [
+                {
+                    name: 'Heart Beet',
+                    description: 'A heart-shaped beet',
+                    type: 'heart-beet',
+                    carryable: true,
+                    walkable: true,
+                    interactions: [],
+                    attributes: [],
+                    on_tick: []
+                }
+            ],
+            mob_types: []
+        };
+        const itemGenerator = new ItemGenerator(worldDescription.item_types);
+        //const world = new ServerWorld(worldDescription);
+        const position = { x: 0, y: 0 };
+        itemGenerator.createItem({
+            type: 'heart-beet',
+            position
+        });
+        const heartBeetID = Item.getItemIDAt(position);
+        expect(heartBeetID).not.toBeNull();
+        const heartBeet = Item.getItem(heartBeetID!);
+        expect(heartBeet).not.toBeNull();
+        expect(heartBeet!.type).toBe('heart-beet');
     });
-    const heartBeetID = Item.getItemIDAt(position);
-    expect(heartBeetID).not.toBeNull();
-    const heartBeet = Item.getItem(heartBeetID!);
-    expect(heartBeet).not.toBeNull();
-    expect(heartBeet!.type).toBe('heart-beet');
-  });
 });
 
 afterAll(() => {
