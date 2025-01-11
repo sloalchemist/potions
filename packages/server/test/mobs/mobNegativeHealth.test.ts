32df31c5b9c8460c67bc7e25251abd8a81b18527
Kaila Aquino <kailad.aquino@gmail.com>
2025-01-11 15:05:04 -0800

    fix: update to commonSetup parameter format

--- packages/server/test/mobs/mobNegativeHealth.test.ts
+++ packages/server/test/mobs/mobNegativeHealth.test.ts
@@ -6,7 +6,7 @@ import { Community } from '../../src/community/community';
 import { buildAndSaveGraph, constructGraph, initialize } from '@rt-potion/converse';
 
 beforeAll(() => {
-  commonSetup("data/mobNegativeHealth.test.db");
+  commonSetup("mobNegativeHealth.test");
   buildAndSaveGraph('../converse/data/test.db', constructGraph(graph));
   initialize('../converse/data/test.db');
 });
