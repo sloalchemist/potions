9e324b8ddf2d137728492d79d81f80c82f810671
Adam Mikacich <adammikacich@gmail.com>
2025-01-11 17:45:28 -0800

    fix: improve readability of the parseName function and provide helpful alert to user on invalid name input

--- packages/client/src/worldMetadata.ts
+++ packages/client/src/worldMetadata.ts
@@ -62,8 +62,18 @@ export async function changeName(name: string) {
   currentCharacter!.name = name;
 }
 
-export function validateName(name: string | null): boolean {
-  return !!name && name.trim() !== '' && name.length < 11
+/**
+ * Parses a name to ensure it fits on screen. The name must be less than
+ * or equal to 10 characters and not empty. Whitespaces are trimmed.
+ * @param name - The name to parse
+ * @returns The parsed name if valid or null otherwise
+ */
+export function parseName(name: string): string | null {
+  const trimmedName = name.trim();
+  if (trimmedName.length > 0 && trimmedName.length <= 10) {
+    return trimmedName;
+  }
+  return null;
 }
 
 export async function retrieveCharacter() {
