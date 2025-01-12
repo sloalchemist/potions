208883a8e1fd5bc66fb07acb233bbcc3e59bf200
Dylan Starink <dylan@starink.com>
2025-01-12 09:30:06 -0800

    chore(client): switch jest to jsdom environment
    Some tests require mocking the canvas, which]
    requires test to run in the jsdom environment.
    This was set as the default for all tests in client

--- packages/client/jest.config.mjs
+++ packages/client/jest.config.mjs
@@ -1,10 +1,11 @@
 export default {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   moduleFileExtensions: ['ts', 'js'],
   transform: {
     '^.+\\.ts$': 'ts-jest',
   },
+  setupFiles: ['jest-canvas-mock'],
   testMatch: ['**/test/**/*.test.ts'],
   moduleNameMapper: {
     '^phaser$': 'phaser/dist/phaser.js'
