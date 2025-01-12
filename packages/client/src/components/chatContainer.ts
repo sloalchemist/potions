30278ffc0329a39b6c35e88a47faaea94977f43d
James Irwin <jamesirwin0211@gmail.com>
2025-01-11 16:43:27 -0800

    chatContainer refactor

--- packages/client/src/components/chatContainer.ts
+++ packages/client/src/components/chatContainer.ts
@@ -1,3 +1,20 @@
-class chatContainer extends Phaser.GameObjects.Container{
+import Phaser from 'phaser';
+import { Button } from '../components/button';
 
+export class ChatContainer extends Phaser.GameObjects.Container {
+   constructor(
+       scene : Phaser.Scene,
+       x : number,
+       y : number
+   ) {
+       super(scene, x, y);
+       scene.add.existing(this);
+   }
+
+   clearChatOptions(chatButtons: Button[]) {
+       // Clear previous chat buttons
+       chatButtons.forEach((button) => button.destroy());
+       chatButtons = [];
+       console.log("Clearing Chat Options");
+   }
 }
