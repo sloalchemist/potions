4578b7b181d9abc5ca58e85628f25911ff5aa2c7
AkashPraveen <46631616+AkashPraveen@users.noreply.github.com>
2025-01-12 13:38:04 -0800

    fix: Potion Stand Drops Items on Smash
    Introduces a fix: when potion stand is smashed, it drops the potions and gold it contained.
    
    Authors: Ivan Nghi, Akash Praveen, Darren Hartano

--- packages/server/src/items/smashable.ts
+++ packages/server/src/items/smashable.ts
@@ -1,5 +1,6 @@
 import { Mob } from '../mobs/mob';
 import { Item } from './item';
+import { itemGenerator } from './itemGenerator';
 
 export class Smashable {
   private item: Item;
@@ -16,7 +17,10 @@ export class Smashable {
     return undefined;
   }
 
+  private mob!: Mob; // Add mob as a class property
+
   smashItem(mob: Mob) {
+    this.mob = mob; // Store mob in the class property
     const attackDmg = Math.floor(Math.random() * mob.attack);
     this.changeHealth(-attackDmg);
   }
@@ -24,13 +28,37 @@ export class Smashable {
   changeHealth(amount: number) {
     this.item.changeAttributeBy('health', amount);
     
+    
     if (this.item.getAttribute<number>('health') <= 0) {
-     if(this.item.type == 'potion-stand') {
-        console.log("This is a potion Stand")
-
-     }
-
+      
       this.item.destroy();
+    }
+  }
+
+  destroyPotionStand(){
+    const gold = this.item.getAttribute<number>('gold');
+    const itemsCount = this.item.getAttribute<number>('items');
+    if (this.item.type == 'potion-stand') {
+      if (this.item.getAttribute<number>('gold') > 0){
+        const position = Item.findEmptyPosition(this.mob.position);
+        itemGenerator.createItem({
+          type: 'gold',
+          position,
+          attributes: { amount: this.item.getAttribute<number>('gold') }
+        });
+      }
+      const itemcount = this.item.getAttribute<number>('items');
+      if (itemcount > 0){
+        console.log('drop potion');
+        for (let i = 0; i < itemcount; i++) {
+          const position = Item.findEmptyPosition(this.mob.position);
+          itemGenerator.createItem({
+            type: 'potion',
+            subtype: this.item.subtype,
+            position: position
+          });
+        }
+      }
     }
   }
 }
