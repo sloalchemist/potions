44f4e40d9ecfd4589fc6ccd787804a01ed6d110b
Almas <apervmm@gmail.com>
2025-01-13 10:55:09 -0800

    null

--- packages/client/test/world/addPotion.test.ts
+++ packages/client/test/world/addPotion.test.ts
@@ -1,10 +1,16 @@
 import { collisionListener } from "../../src/world/controller";
 import { SpriteMob } from "../../src/sprite/sprite_mob";
 import { SpriteItem } from "../../src/sprite/sprite_item";
+import { Item } from '../../src/world/item';
 
 describe("Controller Interaction Logic", () => {
 
-    test("Should have Add Potion option, if player is carrying the potion", () => {
-        
-    });
+  
+
+
+
+
+  test("Should have Add Potion option, if player is carrying the potion", () => {
+      
+  });
 });
