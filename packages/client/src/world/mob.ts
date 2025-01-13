7137d6e33bc1b238f505b981519ee40a9890e66b
RyanHu021 <ryanhu021@gmail.com>
2025-01-13 13:10:28 -0800

    clean up comments

--- packages/client/src/world/mob.ts
+++ packages/client/src/world/mob.ts
@@ -57,7 +57,7 @@ export class Mob extends Physical {
   }
 
   changePosition(world: World, newPosition: Coord) {
-    // console.log('changePosition', this.position, newPosition);
+    //console.log('changePosition', this.position, newPosition);
     world.moveMob(this, newPosition);
     this.position = newPosition;
   }
@@ -75,7 +75,6 @@ export class Mob extends Physical {
       const lastStep = this.path[this.path.length - 1];
       const nextItem = world.getItemAt(nextStep.x, nextStep.y);
       if (!!nextItem && !nextItem.isWalkable(this.unlocks)) {
-        console.log(`Next step blocked for ${this.key} by ${nextItem.name}`);
         this.path = world.generatePath(this.unlocks, this.position, lastStep);
       }
 
