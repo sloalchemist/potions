79e31a5f9d777d7a6b3da20ccb427654247d3f55
Jonathan DeBarro <jdebarro@calpoly.edu>
2025-01-12 16:58:49 -0800

    feat: Deleted unnecessary tests

--- packages/server/test/items/itemWalkable.test.ts
+++ packages/server/test/items/itemWalkable.test.ts
@@ -9,7 +9,6 @@ beforeAll(() => {
 });
 
 describe('Cauldron made not walkable', () => {
-
     test('Check that non walkable items cannot be walked into', () => {
         const worldDescription = {
             tiles: [
@@ -44,61 +43,6 @@ describe('Cauldron made not walkable', () => {
         expect(cauldron?.itemType.walkable).toBe(false);
     });
 
-    test('Check you can still brew potions', () => {
-        const worldDescription = {
-            tiles: [
-                [0, 0],
-                [0, 0]
-            ],
-            terrain_types: [],
-            item_types: [
-                {
-                    name: 'Cauldron',
-                    description: 'For mixing potions',
-                    type: 'cauldron',
-                    carryable: true,
-                    walkable: false, // This is what matters
-                    interactions: [],
-                    attributes: [],
-                    on_tick: []
-                },
-                {
-                    name: 'Heart Beet',
-                    description: 'A heart-shaped beet',
-                    type: 'heart-beet',
-                    carryable: true,
-                    walkable: true,
-                    interactions: [
-                        {
-                            "description": "Brew red potion",
-                            "action": "brew",
-                            "while_carried": true,
-                            "requires_item": "cauldron"
-                        },
-                    ],
-                    attributes: [],
-                    on_tick: []
-                  }
-            ],
-            mob_types: []
-        };
-
-        // Item generation is tested elsewhere
-        const itemGenerator = new ItemGenerator(worldDescription.item_types);
-        const cposition = { x: 1, y: 1 };
-        const hposition = { x: 0, y: 1 };
-        itemGenerator.createItem({
-            type: 'cauldron',
-            position: cposition
-        });
-        itemGenerator.createItem({
-            type: 'heart-beet',
-            position: hposition
-        });
-        const cauldronID = Item.getItemIDAt(cposition);
-        const cauldron = Item.getItem(cauldronID!);
-        expect(cauldron?.itemType.walkable).toBe(false);
-    });
 });
 
 afterAll(() => {
