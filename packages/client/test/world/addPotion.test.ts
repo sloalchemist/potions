bbf06c901c320ee43014b29c2045094e4b4f76b1
Almas <apervmm@gmail.com>
2025-01-12 23:30:18 -0800

    Test: start

--- packages/client/test/world/addPotion.test.ts
+++ packages/client/test/world/addPotion.test.ts
@@ -1,2 +1,10 @@
+import { collisionListener } from "../../src/world/controller";
+import { SpriteMob } from "../../src/sprite/sprite_mob";
+import { SpriteItem } from "../../src/sprite/sprite_item";
 
-import { collisionListener } from '../../src/world/controller'; 
+describe("Controller Interaction Logic", () => {
+
+    test("Should have Add Potion option, if player is carrying the potion", () => {
+        
+    });
+});
