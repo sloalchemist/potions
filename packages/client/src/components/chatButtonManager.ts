174f34dc2679ee1001f35fd3681a5293d81f1e69
Cameron Morrow <cameronmorrow@Camerons-MacBook-Air-4.local>
2025-01-13 10:33:21 -0800

    Prep for PR

--- packages/client/src/components/chatButtonManager.ts
+++ packages/client/src/components/chatButtonManager.ts
@@ -12,11 +12,8 @@ export class ChatButtonManager {
         this.chatButtons.push(object);
    }
 
-   // but how could we add buttons to test this? not sure how we are going to do that TBH..
    clearChatOptions() {
-       // Clear previous chat buttons
        this.chatButtons.forEach((button: Button) => button.destroy());
        this.chatButtons = [];
-       console.log("Clearing Chat Options");
    }
 }
