7fba7d836fac0a9cdbbeb88829e2450c02267f0d
Tyler Pham <137601404+TylerNP@users.noreply.github.com>
2025-01-13 19:40:21 -0800

    feat(client): Added health bars for items only for type fence and wall

--- packages/client/src/sprite/sprite_item.ts
+++ packages/client/src/sprite/sprite_item.ts
@@ -15,6 +15,8 @@ export class SpriteItem extends Item {
   templateSprite: Phaser.GameObjects.Sprite | undefined;
   flat: boolean;
   hasPickedupFrame: boolean = false;
+  healthBar?: Phaser.GameObjects.Graphics;
+  maxHealth?: number;
 
   constructor(scene: WorldScene, item: ItemI) {
     super(world, item.id, item.position, scene.itemTypes[item.type]);
@@ -25,10 +27,17 @@ export class SpriteItem extends Item {
     this.house = item.house;
     this.carried_by = item.carried_by;
     this.lock = item.lock;
+    this.healthBar = scene.add.graphics();
+    this.maxHealth = 100;
 
     // copy over all attributes
     for (const key in item.attributes) {
       this.attributes[key] = item.attributes[key];
+    }
+
+    if(this.itemType.layout_type === 'fence' || this.itemType.layout_type === 'wall') {
+      this.healthBar = scene.add.graphics();
+      this.maxHealth = Number(this.attributes['health']);
     }
 
     let x, y;
@@ -164,6 +173,7 @@ export class SpriteItem extends Item {
   destroy(world: World) {
     super.destroy(world);
     //console.log('Destroying item', this.key);
+    this.healthBar?.destroy();
     this.sprite.destroy();
     if (this.priceText) {
       this.priceText.destroy();
@@ -318,8 +328,17 @@ export class SpriteItem extends Item {
     this.animate();
   }
 
+  calculateHealthPercentage() {
+    return (Number(this.attributes['health']) / this.maxHealth!);
+  }
+
+  isBelowMaxHealth() {
+    return (Number(this.attributes['health']) < this.maxHealth!);
+  }
+
   tick(world: World, deltaTime: number) {
     super.tick(world, deltaTime);
+    
     if (this.position) {
       let depth = this.position.y + 0.5;
       if (this.flat) {
