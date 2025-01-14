62a54b7afcb2aaf4c30107838807544354e9d62b
Ethan Swenke <swenke.ethan.us@gmail.com>
2025-01-13 20:12:58 -0800

    removed excess red potion stand test

--- packages/server/test/items/potionEffects.test.ts
+++ packages/server/test/items/potionEffects.test.ts
@@ -6,6 +6,7 @@ import { Item } from '../../src/items/item';
 import { AddItem } from '../../src/items/uses/container/addItem';
 import { Mob } from '../../src/mobs/mob';
 import { ItemGenerator } from '../../src/items/itemGenerator';
+import { Drink } from '../../src/items/uses/drink';
 
 beforeEach(() => {
   commonSetup();
@@ -13,7 +14,7 @@ beforeEach(() => {
 });
 
 describe('Try to add various color potions to a blue potion-stand', () => {
-  test('Add a blue potion: Should add the potion', () => {
+  test('Create player, consumer blue potions, check increased speed.', () => {
     const worldDescription = {
       tiles: [
         [-1, -1],
@@ -29,192 +30,6 @@ describe('Try to add various color potions to a blue potion-stand', () => {
           walkable: true,
           interactions: [],
           attributes: [],
-          on_tick: []
-        },
-
-        {
-          name: 'Potion stand',
-          description: 'A stand that sells health potions.',
-          type: 'potion-stand',
-          carryable: false,
-          smashable: true,
-          walkable: true,
-          show_price_at: {
-            x: 7,
-            y: -10
-          },
-
-          subtype: '255',
-          interactions: [
-            {
-              description: 'Add $item_name',
-              action: 'add_item',
-              while_carried: false
-            }
-          ],
-          attributes: [
-            {
-              name: 'items',
-              value: 0
-            },
-            {
-              name: 'price',
-              value: 10
-            },
-            {
-              name: 'gold',
-              value: 0
-            },
-            {
-              name: 'health',
-              value: 1
-            }
-          ],
-          on_tick: []
-        }
-      ],
-      mob_types: [
-        {
-          name: 'Player',
-          description: 'The player',
-          name_style: 'norse-english',
-          type: 'player',
-          health: 100,
-          speed: 2.5,
-          attack: 5,
-          gold: 0,
-          community: 'alchemists',
-          stubbornness: 20,
-          bravery: 5,
-          aggression: 5,
-          industriousness: 40,
-          adventurousness: 10,
-          gluttony: 50,
-          sleepy: 80,
-          extroversion: 50,
-          speaker: true
-        }
-      ],
-      communities: [
-        {
-          id: 'alchemists',
-          name: 'Alchemists guild',
-          description:
-            "The Alchemist's guild, a group of alchemists who study the primal colors and their effects."
-        }
-      ]
-    };
-
-    //set up the world
-    const standPosition = { x: 0, y: 1 };
-    const position = { x: 0, y: 0 };
-    mobFactory.loadTemplates(worldDescription.mob_types);
-
-    //create a blue potion stand
-    const itemGenerator = new ItemGenerator(worldDescription.item_types);
-    itemGenerator.createItem({
-      type: 'potion-stand',
-      subtype: '255',
-      position: standPosition,
-      attributes: {
-        templateType: 'potion'
-      }
-    });
-    const standID = Item.getItemIDAt(standPosition);
-    expect(standID).not.toBeNull();
-    const testStand = Item.getItem(standID!);
-    expect(testStand).not.toBeNull();
-
-    // create a player
-    mobFactory.makeMob('player', position, 'TestID', 'TestPlayer');
-    const testMob = Mob.getMob('TestID');
-    expect(testMob).not.toBeNull();
-
-    // create a potion
-    itemGenerator.createItem({
-      type: 'potion',
-      subtype: '255',
-      position: { x: 1, y: 0 },
-      carriedBy: testMob
-    });
-    const potion = Item.getItemIDAt({ x: 1, y: 0 });
-    expect(potion).not.toBeNull();
-    const potionItem = Item.getItem(potion!);
-    expect(potionItem).not.toBeNull();
-
-    // ensure the player is carrying the potion
-    expect(testMob!.carrying).not.toBeNull();
-    expect(testMob!.carrying!.type).toBe('potion');
-    expect(testMob!.carrying!.subtype).toBe('255');
-
-    // add the potion to the stand
-    const testAddItem = new AddItem();
-    const test = testAddItem.interact(testMob!, testStand!);
-    expect(test).toBe(true);
-
-    // check that the potion was added
-    const standAfter = Item.getItem(standID!);
-    expect(standAfter).not.toBeNull();
-    expect(standAfter!.getAttribute('items')).toBe(1);
-  });
-
-  test('Add a red potion: Should NOT add the potion', () => {
-    const worldDescription = {
-      tiles: [
-        [-1, -1],
-        [-1, -1]
-      ],
-      terrain_types: [],
-      item_types: [
-        {
-          name: 'Potion',
-          description: 'A magical concoction',
-          type: 'potion',
-          carryable: true,
-          walkable: true,
-          interactions: [],
-          attributes: [],
-          on_tick: []
-        },
-
-        {
-          name: 'Potion stand',
-          description: 'A stand that sells health potions.',
-          type: 'potion-stand',
-          carryable: false,
-          smashable: true,
-          walkable: true,
-          show_price_at: {
-            x: 7,
-            y: -10
-          },
-
-          subtype: '255',
-          interactions: [
-            {
-              description: 'Add $item_name',
-              action: 'add_item',
-              while_carried: false
-            }
-          ],
-          attributes: [
-            {
-              name: 'items',
-              value: 0
-            },
-            {
-              name: 'price',
-              value: 10
-            },
-            {
-              name: 'gold',
-              value: 0
-            },
-            {
-              name: 'health',
-              value: 1
-            }
-          ],
           on_tick: []
         }
       ],
@@ -280,7 +95,13 @@ describe('Try to add various color potions to a blue potion-stand', () => {
       expect(testMob!.carrying!.subtype).toBe('255');
 
       // have the player drink the potion
-      let test
+      const testDrink = new Drink();
+      const test = testDrink.interact(testMob!, potionItem!);
+      expect(test).toBe(true);
+
+      // check new speed on player
+      
+
   });
 });
 
