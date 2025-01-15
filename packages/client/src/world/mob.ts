8947b15bf76045ced05c290d20c0822ea7741b37
Jackson Isidor <108153124+jacksonisidor@users.noreply.github.com>
2025-01-14 16:14:07 -0800

    Deleted speed where commented out

--- packages/client/src/world/mob.ts
+++ packages/client/src/world/mob.ts
@@ -5,7 +5,6 @@ import { World } from './world';
 export class Mob extends Physical {
   _target?: Coord;
   path: Coord[] = [];
-  // speed: number;
   name: string;
   maxHealth: number;
   dead: boolean = false;
@@ -21,11 +20,9 @@ export class Mob extends Physical {
     type: string,
     maxHealth: number,
     position: Coord | null,
-    // speed: number,
     attributes: Record<string, number>
   ) {
     super(world, key, type, position);
-    // this.speed = speed;
     this.name = name;
     this.maxHealth = maxHealth;
 
