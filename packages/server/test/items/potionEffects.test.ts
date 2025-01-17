2f45198697d3181503e2ce972c7c964948d1d053
smbock42 <smbock42@gmail.com>
2025-01-17 10:17:09 -0800

    Refactor: Remove redundant worldDescription setup in potionEffects tests for cleaner code

--- packages/server/test/items/potionEffects.test.ts
+++ packages/server/test/items/potionEffects.test.ts
@@ -1,4 +1,4 @@
-import { commonSetup } from '../testSetup';
+import { commonSetup, world, itemGenerator } from '../testSetup';
 import { DB } from '../../src/services/database';
 import { mobFactory } from '../../src/mobs/mobFactory';
 import { Community } from '../../src/community/community';
@@ -9,66 +9,14 @@ import { Drink } from '../../src/items/uses/drink';
 
 beforeEach(() => {
   commonSetup();
+  mobFactory.loadTemplates(world.mobTypes);
   Community.makeVillage('alchemists', 'Alchemists guild');
 });
 
 describe('Try to consume blue potion in various cases', () => {
   test('Create player, consume blue potion, then check attributes', () => {
-    const worldDescription = {
-      tiles: [
-        [-1, -1],
-        [-1, -1]
-      ],
-      terrain_types: [],
-      item_types: [
-        {
-          name: 'Potion',
-          description: 'A magical concoction',
-          type: 'potion',
-          carryable: true,
-          walkable: true,
-          interactions: [],
-          attributes: [],
-          on_tick: []
-        }
-      ],
-      mob_types: [
-        {
-          name: 'Player',
-          description: 'The player',
-          name_style: 'norse-english',
-          type: 'player',
-          health: 100,
-          speed: 2.5,
-          attack: 5,
-          gold: 0,
-          community: 'alchemists',
-          stubbornness: 20,
-          bravery: 5,
-          aggression: 5,
-          industriousness: 40,
-          adventurousness: 10,
-          gluttony: 50,
-          sleepy: 80,
-          extroversion: 50,
-          speaker: true
-        }
-      ],
-      communities: [
-        {
-          id: 'alchemists',
-          name: 'Alchemists guild',
-          description:
-            "The Alchemist's guild, a group of alchemists who study the primal colors and their effects."
-        }
-      ]
-    };
 
-    //set up the world
     const position = { x: 0, y: 0 };
-    mobFactory.loadTemplates(worldDescription.mob_types);
-
-    const itemGenerator = new ItemGenerator(worldDescription.item_types);
 
     // create a player
     mobFactory.makeMob('player', position, 'TestID', 'TestPlayer');
@@ -137,61 +85,8 @@ describe('Try to consume blue potion in various cases', () => {
   });
 
   test('Create player with near max speed, consume blue potion, then check attributes', () => {
-    const worldDescription = {
-      tiles: [
-        [-1, -1],
-        [-1, -1]
-      ],
-      terrain_types: [],
-      item_types: [
-        {
-          name: 'Potion',
-          description: 'A magical concoction',
-          type: 'potion',
-          carryable: true,
-          walkable: true,
-          interactions: [],
-          attributes: [],
-          on_tick: []
-        }
-      ],
-      mob_types: [
-        {
-          name: 'Player',
-          description: 'The player',
-          name_style: 'norse-english',
-          type: 'player',
-          health: 100,
-          speed: 9,
-          attack: 5,
-          gold: 0,
-          community: 'alchemists',
-          stubbornness: 20,
-          bravery: 5,
-          aggression: 5,
-          industriousness: 40,
-          adventurousness: 10,
-          gluttony: 50,
-          sleepy: 80,
-          extroversion: 50,
-          speaker: true
-        }
-      ],
-      communities: [
-        {
-          id: 'alchemists',
-          name: 'Alchemists guild',
-          description:
-            "The Alchemist's guild, a group of alchemists who study the primal colors and their effects."
-        }
-      ]
-    };
 
-    //set up the world
     const position = { x: 0, y: 0 };
-    mobFactory.loadTemplates(worldDescription.mob_types);
-
-    const itemGenerator = new ItemGenerator(worldDescription.item_types);
 
     // create a player
     mobFactory.makeMob('player', position, 'TestID', 'TestPlayer');
