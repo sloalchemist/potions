3abeedd268cb3e1fa9b5658217836eb234733363
sgill33 <sgill33@calpoly.edu>
2025-01-25 17:49:54 -0800

    feat: created cauldron class

--- packages/server/src/items/cauldron.ts
+++ packages/server/src/items/cauldron.ts
@@ -1,72 +1,87 @@
 import { itemGenerator } from './itemGenerator';
 import { Mob } from '../mobs/mob';
 import { Item } from './item';
+import { numberToHexString, combineHexColors, hexStringToNumber } from '../util/colorUtil';
 
 export class Cauldron {
   private item: Item;
+
 
   private constructor(item: Item) {
     this.item = item;
   }
   // attributes: num_items, current_potion (based of color addition)
 
-//   static fromItem(item: Item): Container | undefined {
-//     if (item.hasAttribute('items') && item.hasAttribute('templateType')) {
-//       return new Container(item);
-//     }
-
-//     return undefined;
-//   }
-
-//   getType(): string {
-//     return this.item.getAttribute('templateType');
-//   }
-
-//   getInventory(): number {
-//     return this.item.getAttribute('items');
-//   }
+  static fromItem(item: Item): Cauldron | undefined {
+    if (item.hasAttribute('num_items') && item.hasAttribute('potion_subtype')) {
+      return new Cauldron(item);
+    }
+    return undefined;
+  }
 
   getNumItems(): number {
     return this.item.getAttribute('num_items');
   }
 
-  getCurrentPotion(): string {
-    return this.item.getAttribute('current_potion');
+  getPotionSubtype(): string {
+    return this.item.getAttribute('potion_subtype');
   }
 
-  placeItem(mob: Mob): boolean {
+  //TODO: add dump method
+
+  addIngredient(mob: Mob): boolean {
     const carriedItem = mob.carrying;
 
-    if (carriedItem && carriedItem.type === this.getType()) {
-      if (carriedItem.subtype != this.item.subtype) {
-        return false;
-      }
-      carriedItem.destroy();
-      this.item.changeAttributeBy('items', 1);
+    if (carriedItem && carriedItem.hasAttribute("brew_color") && this.getNumItems() <= 3) {
 
+      // if no items in cauldron, add item
+      if (this.getPotionSubtype() == "") {
+        this.item.setAttribute('potion_subtype', String(hexStringToNumber(carriedItem.getAttribute('brew_color'))));
+        this.item.changeAttributeBy('num_items', 1);
+        carriedItem.destroy();
+
+        console.log("Added first ingredient to cauldron");
+        console.log("Cauldron now has " + this.getNumItems() + " items");
+        console.log("Cauldron now has color " + this.getPotionSubtype());
+        return true;
+      }
+
+      //convert subtype to hex string
+      const hexString = numberToHexString(Number(this.getPotionSubtype()));
+
+      //combine hex colors
+      const newColor = combineHexColors(this.getPotionSubtype(), hexString);
+
+      // destroy carried item
+      carriedItem.destroy();
+
+      this.item.changeAttributeBy('num_items', 1);
+      this.item.setAttribute('potion_subtype', newColor);
+
+      console.log("Added ingredient to cauldron");
+      console.log("Cauldron now has " + this.getNumItems() + " items");
+      console.log("Cauldron now has color " + this.getPotionSubtype());
       return true;
     }
 
     return false;
   }
 
-  retrieveItem(mob: Mob): boolean {
-    if (this.getInventory() <= 0) {
+  bottlePotion(mob: Mob): boolean {
+    if (this.getNumItems() <= 0) {
       return false;
     }
 
-    this.item.changeAttributeBy('items', -1);
+    this.item.changeAttributeBy('num_items', -1);
 
     itemGenerator.createItem({
-      type: this.getType(),
-      subtype: this.item.subtype,
+      type: 'potion',
+      subtype: this.getPotionSubtype(),
       carriedBy: mob
     })
 
+    console.log("Bottled potion of type " + this.getPotionSubtype());
+
     return true;
   }
-
-    brewPotion(item:Item): void{
-        return 
-    }
 }
