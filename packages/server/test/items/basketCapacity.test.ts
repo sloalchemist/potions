0d802bb0a5510b90a3cd1544dca8a7723e2e66df
legion5184 <96603312+legion5184@users.noreply.github.com>
2025-01-27 10:14:35 -0800

    Added updated test

--- packages/server/test/items/basketCapacity.test.ts
+++ packages/server/test/items/basketCapacity.test.ts
@@ -1,4 +1,4 @@
-import { commonSetup } from '../testSetup';
+import { commonSetup, village } from '../testSetup';
 import { DB } from '../../src/services/database';
 import { mobFactory } from '../../src/mobs/mobFactory';
 import { Item } from '../../src/items/item';
@@ -6,6 +6,7 @@ import { ItemGenerator } from '../../src/items/itemGenerator';
 import { AddItem } from '../../src/items/uses/container/addItem';
 import { Mob } from '../../src/mobs/mob';
 import { Community } from '../../src/community/community';
+import { log } from 'console';
 
 /* 
     Tests if player is able to add more than 10 logs to basket.
@@ -17,70 +18,6 @@ beforeEach(() => {
   Community.makeVillage('alchemists', 'Alchemists guild');
 });
 
-const worldDescription = {
-  tiles: [
-    [-1, -1],
-    [-1, -1]
-  ],
-  terrain_types: [],
-  item_types: [
-    {
-      name: 'Basket',
-      description: 'test',
-      type: 'basket',
-      carryable: true,
-      smashable: true,
-      walkable: true,
-      interactions: [],
-      attributes: [],
-      on_tick: []
-    },
-    {
-      name: 'Log',
-      description: 'test',
-      type: 'log',
-      carryable: true,
-      smashable: true,
-      walkable: true,
-      interactions: [],
-      attributes: [],
-      on_tick: []
-    },
-  ],
-  mob_types: [
-    {
-      name: 'Player',
-      description: 'The player',
-      name_style: 'norse-english',
-      type: 'player',
-      health: 100,
-      speed: 2.5,
-      attack: 5,
-      gold: 0,
-      community: 'alchemists',
-      stubbornness: 20,
-      bravery: 5,
-      aggression: 5,
-      industriousness: 40,
-      adventurousness: 10,
-      gluttony: 50,
-      sleepy: 80,
-      extroversion: 50,
-      speaker: true
-    } 
-  ],
-  communities: [
-    {
-      id: 'alchemists',
-      name: 'Alchemists guild',
-      description:
-        "The Alchemist's guild, a group of alchemists who study the primal colors and their effects."
-    }
-  ],
-  "containers": [
-    { "type": "basket", "coord": { "x": 14, "y": 38 }, "community": "alchemists", "itemType": "log", "count": 0, "capacity": 10 }
-  ],
-};
 
 describe('Try to add logs to basket until max capacity is reached', () => {
     // world setup
@@ -88,13 +25,24 @@ describe('Try to add logs to basket until max capacity is reached', () => {
     const playerPosition = { x: 0, y: 1};
     mobFactory.loadTemplates(worldDescription.mob_types);
 
+    test('check something', () => {
+      expect(1).toBe(1);
+    })
+    
+
     // create basket
     const itemGenerator = new ItemGenerator(worldDescription.item_types);
     itemGenerator.createItem({
       type: 'basket',
-      position: basketPosition
+      position: basketPosition,
+      ownedBy: village,
+      attributes: {
+        templateType: 'log',
+        items: 0,
+        capacity: 10
+      }
     });
-    const basketID = Item.getItemIDAt(basketPosition);
-    expect(basketID).not.toBeNull();
+    //const basketID = Item.getItemIDAt(basketPosition);
+    //expect(basketID).not.toBeNull();
 
 });
