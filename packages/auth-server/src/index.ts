04877e24724f58b3407f07031611925cb92d9e4e
KyleQ1 <kylequinlan09@gmail.com>
2025-01-27 14:43:35 -0800

    Update character in auth-server

--- packages/auth-server/src/index.ts
+++ packages/auth-server/src/index.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 import corsMiddleware from './corsMiddleware';
 import authController from './authController';
+import characterData from './characterData';
 
 const app = express();
 
 app.use(corsMiddleware);
 
 app.get('/auth', authController);
+
+app.put('/character/:Id', characterData); // PUT
 
 app.listen(3000, () => {
   console.log("I'm listening yo!");
