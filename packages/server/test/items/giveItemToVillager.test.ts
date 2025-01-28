324255db8af1adfcff31d50251731378d76e449b
gabrielriedel <gaberiedel@gmail.com>
2025-01-27 19:15:06 -0800

    chore: fix linting issues

--- packages/server/test/items/giveItemToVillager.test.ts
+++ packages/server/test/items/giveItemToVillager.test.ts
@@ -10,7 +10,9 @@ import { Coord } from '@rt-potion/common';
 beforeEach(() => {
   commonSetup();
   const alchemistVil = Community.makeVillage('alchemists', 'Alchemists guild');
-  const silVil = Community.makeVillage('silverclaw', 'Village of the Silverclaw');
+  const silVil = Community.makeVillage(
+    'silverclaw', 
+    'Village of the Silverclaw');
   // One of the four alliances in the game
   Community.makeAlliance(alchemistVil, silVil)
   mobFactory.loadTemplates(world.mobTypes);
