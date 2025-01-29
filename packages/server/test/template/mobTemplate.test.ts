cc33cdae6b1ff97e6d291ede1becf319899d4f9c
smbock42 <smbock42@gmail.com>
2025-01-29 11:43:52 -0800

    chore: lint file

--- packages/server/test/template/mobTemplate.test.ts
+++ packages/server/test/template/mobTemplate.test.ts
@@ -1,16 +1,16 @@
 // Taken from mob.test.ts
 
-import { commonSetup, world } from "../testSetup";
-import { Mob } from "../../src/mobs/mob";
-import { mobFactory } from "../../src/mobs/mobFactory";
-import { Coord } from "@rt-potion/common";
-import { Community } from "../../src/community/community";
-import { DB } from "../../src/services/database";
+import { commonSetup, world } from '../testSetup';
+import { Mob } from '../../src/mobs/mob';
+import { mobFactory } from '../../src/mobs/mobFactory';
+import { Coord } from '@rt-potion/common';
+import { Community } from '../../src/community/community';
+import { DB } from '../../src/services/database';
 
 beforeEach(() => {
   commonSetup();
   Community.makeVillage('alchemists', 'Alchemists guild');
-  mobFactory.loadTemplates(world.mobTypes)
+  mobFactory.loadTemplates(world.mobTypes);
 });
 
 describe('Mob Tests', () => {
@@ -19,17 +19,17 @@ describe('Mob Tests', () => {
       const position: Coord = { x: 0, y: 0 };
       const mobId = 'testmob';
 
-      mobFactory.makeMob('player', position, mobId, 'testPlayer') // Create mob
+      mobFactory.makeMob('player', position, mobId, 'testPlayer'); // Create mob
       const testMob = Mob.getMob(mobId); // get mob
 
       expect(testMob).toBeDefined(); // do tests on the mob here...
     });
-  })
+  });
 
   describe('Mob Health Behavior', () => {
-    test("Health decreases correctly and does not drop below zero", () => {
+    test('Health decreases correctly and does not drop below zero', () => {
       const mobId = 'testmob-health';
-      const playerPosition: Coord = { x: 0, y: 0 }
+      const playerPosition: Coord = { x: 0, y: 0 };
 
       mobFactory.makeMob('player', playerPosition, mobId, 'testPlayer'); // make mob
       const testMob = Mob.getMob(mobId); // get mob
@@ -38,10 +38,9 @@ describe('Mob Tests', () => {
       testMob?.changeHealth(-50); // decrease mob health
       expect(testMob?.health).toBe(50);
 
-      //health 50 - 60 is < 0, so should be 0. 
+      //health 50 - 60 is < 0, so should be 0
       testMob?.changeHealth(-60);
       expect(testMob?.health).toBe(0);
-
     });
   });
 });
