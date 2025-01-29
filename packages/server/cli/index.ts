ea2e96fbd2688fd55e44c32920df47dd52047eb8
Ryan Chan <ryanchansf@gmail.com>
2025-01-29 00:21:47 -0800

    feat: incomplete script for mob spawning

---
+++ packages/server/cli/index.ts
@@ -0,0 +1,85 @@
+const readline = require('readline');
+import { mobFactory } from "../src/mobs/mobFactory";
+import globalData from "../data/global.json";
+
+// if glo
+// import { itemGenerator } from "../src/items/itemGenerator";
+
+// Setup `readline` for interactive CLI
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "game-server> ",
+});
+
+const handleCliCommand = (input: string) => {
+    const [command, entityType, name, ...args] = input.trim().split(" ");
+    console.log(input);
+    if (input.trim() === "exit") {
+        rl.close();
+        return;
+    } else if (input.trim() === "help") {
+        console.log(`
+Available commands:
+- spawn mob [type] x:[x-coord] y:[y-coord]
+- spawn item [type] count:[number]
+- exit: Quit CLI
+    `);
+  } else if (command === "spawn") {
+    const attributes: Record<string, string | number> = {};
+    args.forEach(arg => {
+        const [key, value] = arg.split(":");
+        attributes[key] = isNaN(Number(value)) ? value : Number(value);
+    });
+    console.log("args", args)
+    console.log("name", name)
+    console.log("attributes", attributes)
+    console.log("entityType", entityType)
+
+    switch (entityType) {
+
+        case "mob":
+            if (["blob", "villager", "fighter"].includes(name)) {
+                const { x, y } = attributes;
+                mobFactory.makeMob(name, { x: x as number, y: y as number });
+                
+                console.log(`Spawned mob: ${name} at (${x}, ${y})`);
+            } else {
+                console.log("Unknown mob type.");
+            }
+            break;
+        case "item":
+            if (["items"].includes(name)) {
+                const { count } = attributes;
+                // add spawn item logic
+                console.log(`Spawned ${count}x ${entityType}`);
+            } else {
+                console.log("Unknown entity type.");
+            }
+            break;
+    }
+
+  } else {
+        console.log("Invalid command. Type 'help' for available commands.");
+  }
+
+    rl.prompt();
+}
+
+rl.on("close", () => {
+  console.log("Exiting CLI. Game server still running.");
+});
+
+
+export const startCli = () => {
+    console.log("Game server running...");
+    
+    console.log("Game Cheat CLI enabled. Type 'help' for commands.");
+    rl.prompt();
+
+    rl.on("line", handleCliCommand);
+
+    rl.on("close", () => {
+        console.log("Exiting CLI.");
+    });
+}
--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,3 +1,7 @@
 import { worldTimer } from './services/setup';
+import { startCli } from '../cli/index';
 
 setInterval(worldTimer, 500);
+
+// start listening for CLI commands
+startCli();
