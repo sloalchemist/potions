1f6fe768b5fe67962d34fffd78b32c7b77920fa2
James Irwin <jamesirwin0211@gmail.com>
2025-01-29 18:13:06 -0800

    fix: clear tiles, terrain_types

--- packages/server/src/generate/buildWorld.ts
+++ packages/server/src/generate/buildWorld.ts
@@ -8,15 +8,14 @@ import { createTables, loadDefaults } from './generateWorld';
 import { StubbedPubSub } from '../services/clientCommunication/stubbedPubSub';
 import { initializePubSub } from '../services/clientCommunication/pubsub';
 import { buildGraphFromWorld } from './socialWorld';
-
 import globalData from '../../data/global.json';
 import worldSpecificData from '../../data/world_specific.json';
-
 import { ServerWorldDescription } from '../services/gameWorld/worldMetadata';
 import { initializeGameWorld } from '../services/gameWorld/gameWorld';
 import { ServerWorld } from '../services/gameWorld/serverWorld';
 
 async function main() {
+  // Build and save the knowledge graph
   // Initialize the server database
   await initializeServerDatabase('data/server-data.db', true);
 
