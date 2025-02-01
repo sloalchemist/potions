4ceeff68f940de31d68b94e79f3e57d4f208d8a0
Ethan Vosburg <63841608+EthanV1920@users.noreply.github.com>
2025-01-31 23:01:58 -0800

    Corrected enviroment typo

--- .terraform-prod/main.tf
+++ .terraform-prod/main.tf
@@ -209,7 +209,7 @@ data "github_repository" "repo" {
 
 resource "github_repository_environment" "repo_environment" {
   repository       = data.github_repository.repo.name
-  environment      = "github_pages"
+  environment      = "github-pages"
 }
 
 resource "github_actions_environment_variable" "server_url" {
