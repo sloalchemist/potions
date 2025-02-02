96292d6707b3b9bd730657c351c1f54b4dd19530
logancostello <locostel@calpoly.edu>
2025-02-01 19:38:48 -0800

    feat: added file uploading to supabase

--- packages/server/src/services/supabaseStorage.ts
+++ packages/server/src/services/supabaseStorage.ts
@@ -1,18 +1,25 @@
 import { supabase } from "../../../auth-server/src/authController"
 
+export async function downloadFile(file: string) {
+    if (!process.env.supabase_bucket) {
+        throw Error("Your server env needs the SUPABASE_BUCKET var. Check README for info")
+    }
 
-async function downloadFile(file: string) {
     const { data, error } = await supabase
         .storage
-        .from('world-db-data')
+        .from(process.env.supabase_bucket)
         .download(file);
 }
 
 
-async function uploadFile(file: File, filePath: string) {
+export async function uploadFile(file: File, filePath: string) {
+    if (!process.env.supabase_bucket) {
+        throw Error("Your server env needs the SUPABASE_BUCKET var. Check README for info")
+    }
+
     const { data, error } = await supabase
         .storage
-        .from('world-db-data')
+        .from(process.env.supabase_bucket)
         .upload(filePath, file, {
             cacheControl: '3600',
             upsert: false
