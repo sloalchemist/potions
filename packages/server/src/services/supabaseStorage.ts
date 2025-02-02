b0d7b6373c7efc5c37c37d282fbc2256d79f7dab
Jonathan DeBarro <jdebarro@calpoly.edu>
2025-02-01 19:34:23 -0800

    feat: download file functionality

--- packages/server/src/services/supabaseStorage.ts
+++ packages/server/src/services/supabaseStorage.ts
@@ -1,11 +1,36 @@
+import fs from 'fs';
 import { supabase } from "../../../auth-server/src/authController"
 
 
 async function downloadFile(file: string) {
+    // File stored in data as Blob object
     const { data, error } = await supabase
         .storage
         .from('world-db-data')
         .download(file);
+    
+    if (error) {
+        console.error('Error downloading db file:', error.message);
+        return;
+    }
+
+    // Convert Blob to file
+    const myFile = new File([data], file, {
+        type: data.type,
+        lastModified: new Date().getTime()
+    });
+
+    // Rename file, moving to to data location
+    var path = require('path');
+    var newPath = path.join('..', 'data', file);
+    var oldPath = path.join('.', file);
+    fs.rename(oldPath, newPath, (err) => {
+        if (err) {
+            console.error('Error moving file:', err);
+        } else {
+            console.log('File moved successfully!');
+        }
+    });
 }
 
 
@@ -18,3 +43,5 @@ async function uploadFile(file: File, filePath: string) {
             upsert: false
         });
 }
+
+export { uploadFile, downloadFile };
