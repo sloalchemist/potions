b0aa781400f821fb27f848c4cb0bed2c0c3617df
sgill33 <sgill33@calpoly.edu>
2025-02-03 14:54:33 -0800

    fix: renamed image to avoid conflicts

--- packages/client/src/scenes/brewScene.ts
+++ packages/client/src/scenes/brewScene.ts
@@ -9,14 +9,14 @@ export class BrewScene extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image('overlayImage', 'static/potion-brewing.png');
+    this.load.image('brewImage', 'static/potion-brewing.png');
   }
 
   create() {
     const overlayImage = this.add.image(
       SCREEN_WIDTH / 2,
       SCREEN_HEIGHT / 2,
-      'overlayImage'
+      'brewImage'
     ); // Adjust x, y and texture as needed
     overlayImage.setScrollFactor(0); // Prevent image from moving when scenes scroll
     console.log('BrewScene created');
