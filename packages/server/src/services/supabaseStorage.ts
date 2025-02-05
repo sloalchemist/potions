2cbc87bc8ae97cb5393a953ba9eeb19782de1c50
logancostello <locostel@calpoly.edu>
2025-02-05 15:15:13 -0800

    fix: upload time from 1 min to 10 mins

--- packages/server/src/services/supabaseStorage.ts
+++ packages/server/src/services/supabaseStorage.ts
@@ -112,7 +112,7 @@ async function uploadLocalData() {
   }
 
 function shouldUploadDB(now: number, lastUpdated: number) {
-    const interval = 60000;
+    const interval = 600000;
     return now - lastUpdated >= interval;
 };
 
