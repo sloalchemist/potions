df72b375451605e0d0e300bcf64bbf098fa9e374
logancostello <locostel@calpoly.edu>
2025-02-05 15:14:46 -0800

    fix: updated test to use different func definition

--- packages/server/test/supabaseStorage/supabaseStorage.test.ts
+++ packages/server/test/supabaseStorage/supabaseStorage.test.ts
@@ -1,31 +1,13 @@
-import * as fs from 'fs'
-import { lastUpdated, shouldUploadDB } from "../../src/services/supabaseStorage";
-
-
-async function fileCreate() {
-  const fileBufferServer = await fs.promises.readFile("data/server-data.db", 'utf-8')
-  const blobServer = new Blob([fileBufferServer], { type: "binary/octet-stream" });
-  const fileServer = new File([blobServer], "server-data.db", {
-      type: blobServer.type,
-      lastModified: new Date().getTime()
-  });
-    return fileServer;
-}
+import { shouldUploadDB } from "../../src/services/supabaseStorage";
 
 describe('Upload and Download Supabase Bucket', () => {
 
     test('Test upload to S3 bucket', () => {
-        const currentTime = Date.now();
-        const uploadTime = Date.now() + 600001;
-        expect(shouldUploadDB(currentTime)).toBeFalsy();
-        expect(shouldUploadDB(uploadTime)).toBeTruthy();
-    });
-    
-    test('Test upload to S3 bucket', () => {
-        const currentTime = Date.now();
-        const uploadTime = Date.now() + 600001;
-        expect(shouldUploadDB(currentTime)).toBeFalsy();
-        expect(shouldUploadDB(uploadTime)).toBeTruthy();
+        const now = Date.now();
+        const aMinBefore = now - 60000;
+        const tenMinBefore = now - 600001
+        expect(shouldUploadDB(now, aMinBefore)).toBeFalsy();
+        expect(shouldUploadDB(now, tenMinBefore)).toBeTruthy();
     });
 });
 
