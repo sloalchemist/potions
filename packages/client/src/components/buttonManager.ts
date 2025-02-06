19bb4ab9ad94087ac9cdc4919526be5281537685
sgill33 <sgill33@calpoly.edu>
2025-02-05 19:49:23 -0800

    feat: replaced get method with method to remove buttons that don't match a texture

--- packages/client/src/components/buttonManager.ts
+++ packages/client/src/components/buttonManager.ts
@@ -10,12 +10,21 @@ export class ButtonManager {
     this.buttons.push(object);
   }
 
-  getButtons() {
-    return this.buttons;
-  }
-  
   clearButtonOptions() {
     this.buttons.forEach((button: Button) => button.destroy());
     this.buttons = [];
   }
+
+  // Takes a texture string as an argument and removes all buttons that do not have that texture.
+  clearUnmatchedButtons(texture:string) {
+      this.buttons.forEach((button) => {
+        if (button.texture !== texture) {
+          button.destroy();
+        }
+      });
+  }
+
+  
 }
+
+
