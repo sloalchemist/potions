9d82b4bd29118323ac75757a632cf82e02210da4
ddharton <ddharton@calpoly.edu>
2025-02-05 17:08:06 -0800

    I wish for the grim reaper to consume my state of being

--- packages/server/src/items/market.ts
+++ packages/server/src/items/market.ts
@@ -4,28 +4,38 @@ import { Item } from './item';
 
 export class Market {
         
-    private item: Item;
+    private itemlist: Item[];
+    private itemquantity: Number[];
 
-    private constructor(item: Item) {
-        this.item = item;
+    private constructor() {
+        this.itemlist = [];
+        this.itemquantity = [];
     }
 
     static fromItem(item: Item): Market | undefined {
+        /*
+        This should be irrelevant
         if (item.hasAttribute('items') && item.hasAttribute('templateType')) {
         return new Market(item);
         }
-
+        */
         return undefined;
     }
-
+    
     getType(): string {
-        return this.item.getAttribute('templateType');
+        // return this.item.getAttribute('templateType');
+        return "This is a fucking market you imbicile"
     }
 
+    // there is a problem here, looking for number
+    // unsure if this is right. It probably assumes that this is a market of a given type
     getInventory(): number {
-        return this.item.getAttribute('items');
+        // do a list traversal of this shit and run it
+        return ;
     }
 
+    // Item stand cap.
+    // This should be the total number of entries that are used.
     getCapacity(): number {
         return 10;
     }
