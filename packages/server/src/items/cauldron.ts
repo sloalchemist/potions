1691d342ebbd0eddcc5877b4112c2897e4483f55
sgill33 <sgill33@calpoly.edu>
2025-02-08 22:00:18 -0800

    feat: potions are now valid ingredients and hold less color weight

--- packages/server/src/items/cauldron.ts
+++ packages/server/src/items/cauldron.ts
@@ -49,11 +49,24 @@ export class Cauldron {
 
     if (
       carriedItem &&
-      carriedItem.hasAttribute('brew_color') &&
+      (carriedItem.hasAttribute('brew_color') || carriedItem.type == "potion") &&
       this.getNumItems() < 3
     ) {
       // get ingredient color from carried item
-      const ingredientColor : string = carriedItem.getAttribute('brew_color');
+      var ingredientColor : string
+      if (carriedItem.type == 'potion') {
+        ingredientColor = numberToHexString(Number(carriedItem.subtype))
+      }
+      else {
+        ingredientColor = carriedItem.getAttribute('brew_color');
+      }
+    
+      //calculate weight for added ingredient
+      let ingredientWeight = 1
+      if(carriedItem.type == 'potion'){
+        ingredientWeight = 0.5; //change depending on how much you want potions to affect color
+      }
+
       // if no items in cauldron, add item
       if (Number(this.getPotionSubtype()) == 0) {
         this.item.setAttribute(
@@ -62,6 +75,7 @@ export class Cauldron {
         );
         this.item.changeAttributeBy('ingredients', 1);
         carriedItem.destroy();
+        this.item.setAttribute('color_weight', ingredientWeight);
         return true;
       }
 
@@ -73,17 +87,17 @@ export class Cauldron {
         hexString,
         ingredientColor,
         this.getColorWeight(),
-        1
+        ingredientWeight
       );
-
 
       //if output color is the same as the ingredient color, increase weight
       if (newColor == ingredientColor) {
-        this.item.changeAttributeBy('color_weight', 1);
+        this.item.changeAttributeBy('color_weight', ingredientWeight);
       }
       else{
-        this.item.setAttribute('color_weight', 1);
+        this.item.setAttribute('color_weight', ingredientWeight);
       }
+      console.log("ingredient weight: ", ingredientWeight);
 
       // destroy carried item
       carriedItem.destroy();
