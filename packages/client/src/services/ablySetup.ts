be5edb5fdf0aaf5db941d1bd6cb345be48a434b5
smbock42 <smbock42@gmail.com>
2025-02-10 17:46:10 -0800

    chore: fix lint

--- packages/client/src/services/ablySetup.ts
+++ packages/client/src/services/ablySetup.ts
@@ -9,7 +9,7 @@ export let playerChannel: Types.RealtimeChannelCallbacks;
 
 const SERVER_URL = process.env.SERVER_URL;
 let channelsBoundToWorld: boolean = false;
-let authorizer =
+export let authorizer =
   SERVER_URL.slice(-1) == '/' ? 'auth?username=' : '/auth?username=';
 export function setupAbly(): Promise<void> {
   let authorizer =
