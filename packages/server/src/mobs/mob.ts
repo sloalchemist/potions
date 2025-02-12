8e4b62e5d8762c0e36800916b59051d0bbd2d5d8
TheAllenChoi <114026584+TheAllenChoi@users.noreply.github.com>
2025-02-12 14:01:18 -0800

    add: function to update favorability upon initiating fight

--- packages/server/src/mobs/mob.ts
+++ packages/server/src/mobs/mob.ts
@@ -673,8 +673,27 @@ export class Mob {
     // TODO: replace with FightTracker class
     pubSub.playerAttacks(mob.id, ['Test Attack']);
     // fightTracker.startFight(mob, this);
+    this.updateFightFavorability(mob);
     return false;
   }
+
+  /**
+   * Updates mob species' favorability to decrease by 20 with the player
+   * @param mob The target mob whose species you want to decrease favorability with
+   */
+  updateFightFavorability(mob: Mob): void {
+    var id = mob.community_id
+    DB.prepare(
+      `   
+      UPDATE favorability
+        SET favor = favor - 20
+        WHERE
+            (community_1_id = :id_1 AND community_2_id = :id_2) OR
+            (community_1_id = :id_2 AND community_2_id = :id_1)
+        `
+    ).run({ id_1: "alchemists", id_2: id});
+  }
+
 
   static findCarryingMobID(item_id: string): string | undefined {
     const mob = DB.prepare(
