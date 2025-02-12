a05ef6e4005fb20462c3626aabb8f9c52fe9214a
Adam Mikacich <adammikacich@gmail.com>
2025-02-12 15:09:08 -0800

    Implement "Enter Portal" interaction and allow user to select a target world (#339)
    Co-authored-by: kannanjain <kannanjain4@gmail.com>
    Co-authored-by: KyleQ1 <kylequinlan09@gmail.com>

---
+++ packages/auth-server/src/worldController.ts
@@ -0,0 +1,14 @@
+import { supabase } from './authController';
+import { Request, Response } from 'express';
+
+const worldController = async (_req: Request, res: Response) => {
+  const { data, error } = await supabase.from('worlds').select('id, world_id');
+
+  if (error || !data) {
+    return res.status(500).json({ error: 'Failed to fetch worlds' });
+  }
+
+  res.status(200).json(data);
+};
+
+export default worldController;
