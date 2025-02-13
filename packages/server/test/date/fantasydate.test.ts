8ce16213b5f9f39102f2779b74fb775311a6ab59
Ryan Agdassi <113940065+ragdassi@users.noreply.github.com>
2025-02-12 22:39:53 -0800

    fix: added code coverage in server tests (fantasydate)

--- packages/server/test/date/fantasydate.test.ts
+++ packages/server/test/date/fantasydate.test.ts
@@ -1,6 +1,7 @@
-import { FantasyDate } from '../../src/date/fantasyDate';
+import { FantasyDate, MINUTES_IN_HOUR } from '../../src/date/fantasyDate';
 import { commonSetup } from '../testSetup';
 import { DB } from '../../src/services/database';
+import { pubSub } from '../../src/services/clientCommunication/pubsub';
 
 beforeAll(() => {
   commonSetup();
@@ -11,20 +12,33 @@ describe('FantasyDate', () => {
     // set global tick to 0 in DB
     FantasyDate.initialDate();
 
-    // get the current date from the currently stored tick value in DB (should be 0)
-    // this constructs a FantasyDate object in which we set init_date equal to
     let init_date = FantasyDate.currentDate();
 
-    // OUR BUG: this should construct a FantasyDate with day = 1 and month = 1 instead of 0 for better clarity
-    // EXPECTED: 1/1/0 0'oclock
-    // so that, our expected day and month would be 1 instead of 0
     expect(init_date.day).toBe(1);
     expect(init_date.month).toBe(1);
-    // all other fields should be 0
+
     expect(init_date.year).toBe(0);
     expect(init_date.hour).toBe(0);
     expect(init_date.minute).toBe(0);
     expect(init_date.global_tick).toBe(0);
+  });
+
+  test('runTick should call pubSub.setDateTime when minute is 0', () => {
+    // set global tick to a value that will result in minute being 0 after incrementing
+    DB.prepare(`
+      UPDATE ticks
+      SET tick = ${MINUTES_IN_HOUR - 1}
+    `).run();
+
+    // mock pubSub.setDateTime to verify it's called
+    const setDateTimeSpy = jest.spyOn(pubSub, 'setDateTime');
+
+    // run the tick
+    FantasyDate.runTick();
+
+    // verify pubSub.setDateTime was called
+    expect(setDateTimeSpy).toHaveBeenCalledTimes(1);
+    expect(setDateTimeSpy).toHaveBeenCalledWith(FantasyDate.currentDate());
   });
 });
 
