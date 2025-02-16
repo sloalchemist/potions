c7d4377e86e8cff2c45b5b2c530bcadbf8ff19f3
Charlotte Maples <cgmaples13@gmail.com>
2025-02-15 22:53:02 -0800

    feat: update Brew Scene unit tests

--- packages/client/test/scene/brewScene.test.ts
+++ packages/client/test/scene/brewScene.test.ts
@@ -169,6 +169,7 @@ import { UxScene } from '../../src/scenes/uxScene';
 import { Item } from '../../src/world/item';
 import { World } from '../../src/world/world';
 import { Interactions } from '../../src/world/controller';
+import exp from 'constants';
 
 // Start of test suite
 const world = new World();
@@ -196,7 +197,7 @@ beforeAll(() => {
 // A helper function to patch a UxScene instance with the properties it needs
 function patchUxScene(uxScene: UxScene) {
   // Patch the mixContainer using a type assertion
-  uxScene.mixContainer = {
+  uxScene.itemsContainer = {
     add: jest.fn()
   } as unknown as Phaser.GameObjects.Container;
 
@@ -204,7 +205,7 @@ function patchUxScene(uxScene: UxScene) {
     isActive: jest.fn().mockReturnValue(false),
     launch: jest.fn(),
     stop: jest.fn(),
-    get: jest.fn(() => ({ setBrewColor: jest.fn() })) as any
+    get: jest.fn(() => ({ setBrewColor: jest.fn(), setNumIngredients: jest.fn() })) as any
   } as any;
 }
 
@@ -242,13 +243,13 @@ describe('BrewScene actions based on state', () => {
       label: 'label'
     };
 
-    uxScene.setBrewOptions([interaction]);
+    uxScene.setInteractions([interaction]);
 
     //check that toggle button was created
-    expect(uxScene.mixButtons.buttons.length).toEqual(1);
+    expect(uxScene.interactButtons.buttons.length).toEqual(1);
 
     // Confirm the texture of the button is 'Toggle Menu'
-    const toggleButton = uxScene.mixButtons.buttons[0];
+    const toggleButton = uxScene.interactButtons.buttons[0];
     expect(toggleButton.texture).toEqual('Toggle Menu');
   });
 
@@ -278,10 +279,10 @@ describe('BrewScene actions based on state', () => {
       label: 'label'
     };
 
-    uxScene.setBrewOptions([interaction]);
+    uxScene.setInteractions([interaction]);
 
     // Retrieve the toggle button
-    const toggleButton = uxScene.mixButtons.buttons[0];
+    const toggleButton = uxScene.interactButtons.buttons[0];
 
     // Simulate a button click
     toggleButton.emit('pointerdown');
@@ -291,5 +292,139 @@ describe('BrewScene actions based on state', () => {
 
     // Advance timers so that the setTimeout callback is executed
     jest.runAllTimers();
+  });
+});
+
+describe('Buttons shown when BrewScene is and is not active', () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+  afterEach(() => {
+    jest.clearAllTimers();
+  });
+
+  test('Active BrewScene shows brew options', () => {
+    const uxScene = new UxScene();
+    patchUxScene(uxScene);
+    // Ensure isActive returns false
+    uxScene.scene.isActive = jest.fn().mockReturnValue(true);
+
+    const cauldron = new Item(
+      world,
+      'cauldron',
+      { x: 0, y: 0 },
+      {
+        name: 'Cauldron',
+        type: 'cauldron',
+        carryable: false,
+        walkable: false,
+        interactions: [],
+        attributes: { potion_subtype: '654321' } as any
+      }
+    );
+
+    const blueberry = new Item(
+      world,
+      'blueberry',
+      { x: 1, y: 0 },
+      {
+        name: 'Blueberry',
+        type: 'blueberry',
+        carryable: true,
+        walkable: true,
+        interactions: []
+      }
+    );
+
+    const interactions: Interactions[] = [
+      {
+        item: cauldron,
+        action: 'action',
+        label: 'cooked'
+      },
+      {
+        item: blueberry,
+        action: 'munch',
+        label: 'label'
+      }
+    ];
+
+    uxScene.setInteractions(interactions);
+
+    //check that the buttons were created
+    expect(uxScene.interactButtons.buttons.length).toEqual(2);
+
+    console.log(uxScene.interactButtons.buttons);
+
+    // Confirm the texture of the first button is 'cooked'
+    const blueberryButton = uxScene.interactButtons.buttons[0];
+    expect(blueberryButton.texture).toEqual('cooked');
+    
+    // Confirm the texture of the second button is 'Toggle Menu'
+    const toggleButton = uxScene.interactButtons.buttons[1];
+    expect(toggleButton.texture).toEqual('Toggle Menu');
+
+    // Advance timers so that the setTimeout callback is executed
+    jest.runAllTimers();
+  });
+
+  test('Inactive BrewScene shows brew options', () => {
+    const uxScene = new UxScene();
+    patchUxScene(uxScene);
+    // Ensure isActive returns false
+    uxScene.scene.isActive = jest.fn().mockReturnValue(false);
+
+    const cauldron = new Item(
+      world,
+      'cauldron',
+      { x: 0, y: 0 },
+      {
+        name: 'Cauldron',
+        type: 'cauldron',
+        carryable: false,
+        walkable: false,
+        interactions: [],
+        attributes: { potion_subtype: '654321' } as any
+      }
+    );
+
+    const blueberry = new Item(
+      world,
+      'blueberry',
+      { x: 1, y: 0 },
+      {
+        name: 'Blueberry',
+        type: 'blueberry',
+        carryable: true,
+        walkable: true,
+        interactions: []
+      }
+    );
+
+    const interactions: Interactions[] = [
+      {
+        item: cauldron,
+        action: 'no',
+        label: 'label'
+      },
+      {
+        item: blueberry,
+        action: 'action',
+        label: 'omnom'
+      }
+    ];
+
+    uxScene.setInteractions(interactions);
+
+    //check that the buttons were created
+    expect(uxScene.interactButtons.buttons.length).toEqual(2);
+
+    // Confirm the texture of the first button is 'omnom'
+    const blueberryButton = uxScene.interactButtons.buttons[0];
+    expect(blueberryButton.texture).toEqual('omnom');
+    
+    // Confirm the texture of the second button is 'Toggle Menu'
+    const toggleButton = uxScene.interactButtons.buttons[1];
+    expect(toggleButton.texture).toEqual('Toggle Menu');
   });
 });
