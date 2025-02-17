6cb99a48f1e96c048b9476fe5fa0375423c9dbbf
Alfred Madere <45337924+AlfredMadere@users.noreply.github.com>
2025-02-17 10:18:45 -0800

    fix: not throwing env var errors in test env

--- packages/common/src/utils.ts
+++ packages/common/src/utils.ts
@@ -1,6 +1,6 @@
 export function getEnv(name: string) {
   let value = process.env[name];
-  if (!value) {
+  if (!value && process.env.NODE_ENV !== 'test') {
     throw new Error(`Environment variable ${name} is not set.`);
   }
 
