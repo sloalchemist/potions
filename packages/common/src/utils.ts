45e96c7112b4302e729388c353583156495e17ec
Alfred Madere <45337924+AlfredMadere@users.noreply.github.com>
2025-02-17 10:37:30 -0800

    not blowing up when env vars arent setting during testing

--- packages/common/src/utils.ts
+++ packages/common/src/utils.ts
@@ -1,11 +1,11 @@
 export function getEnv(name: string) {
   let value = process.env[name];
   if (!value) {
-    if (process.env.NODE_ENV !== 'test') {
-      return `Environment variable ${name} is not set.`;
+    // Check if we're running in Jest (it sets JEST_WORKER_ID)
+    if (process.env.JEST_WORKER_ID) {
+      return `test-${name}`;
     }
     throw new Error(`Environment variable ${name} is not set.`);
   }
-
   return value;
 }
