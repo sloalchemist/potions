a33e5cee7777d6a326039626519ed7fb0ed59395
sbryukho <sbryukho@calpoly.edu>
2025-02-19 19:28:27 -0800

    read.ts

--- packages/server/src/items/uses/read.ts
+++ packages/server/src/items/uses/read.ts
@@ -1,6 +1,26 @@
 import { Mob } from '../../mobs/mob';
-import { Carryable } from '../carryable';
 import { Item } from '../item';
 import { Use } from './use';
 
-// create read class
+export class Read implements Use {
+  key: string;
+
+  constructor() {
+    this.key = 'read';
+  }
+
+  description(_mob: Mob, _item: Item): string {
+    return 'Read';
+  }
+
+  interact(mob: Mob, item: Item): boolean {
+    if (item.type !== 'message-in-bottle') {
+      return false;
+    }
+
+    const message = "some message here (replace)"; 
+    mob.sendMessage(`You read the message: "${message}"`);
+
+    return true;
+  }
+}
