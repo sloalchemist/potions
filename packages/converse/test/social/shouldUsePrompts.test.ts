2d389ab05514938d54a3db9f287d8273b9dd7d57
logancostello <locostel@calpoly.edu>
2025-02-19 20:14:58 -0800

    linting

--- packages/converse/test/social/shouldUsePrompts.test.ts
+++ packages/converse/test/social/shouldUsePrompts.test.ts
@@ -1,25 +1,25 @@
-import { shouldUsePrompts } from "../../src/library/social/prompts/shouldUsePrompts";
+import { shouldUsePrompts } from '../../src/library/social/prompts/shouldUsePrompts';
 
-describe("Ensure LLM is attempted to be used only when set up correctly", () => {
-    test("Dev does not specify, should be false", () => {
-        expect(shouldUsePrompts()).toBe(false);
-    });
+describe('Ensure LLM is attempted to be used only when set up correctly', () => {
+  test('Dev does not specify, should be false', () => {
+    expect(shouldUsePrompts()).toBe(false);
+  });
 
-    test("Dev opts out, should be false", () => {
-        process.env.LLM_FLAG="false";
-        expect(shouldUsePrompts()).toBe(false);
-    });
+  test('Dev opts out, should be false', () => {
+    process.env.LLM_FLAG = 'false';
+    expect(shouldUsePrompts()).toBe(false);
+  });
 
-    test("Dev opts in but has no credentials, should be false", () => {
-        process.env.LLM_FLAG="true";
-        expect(shouldUsePrompts()).toBe(false);
-    });
+  test('Dev opts in but has no credentials, should be false', () => {
+    process.env.LLM_FLAG = 'true';
+    expect(shouldUsePrompts()).toBe(false);
+  });
 
-    test("Dev opts in and has credentials, should be true", () => {
-        process.env.REDIS_HOST="test";
-        process.env.REDIS_PORT="test";
-        process.env.REDIS_PASSWORD="test";
-        process.env.LLM_FLAG="true";
-        expect(shouldUsePrompts()).toBe(true);
-    });
+  test('Dev opts in and has credentials, should be true', () => {
+    process.env.REDIS_HOST = 'test';
+    process.env.REDIS_PORT = 'test';
+    process.env.REDIS_PASSWORD = 'test';
+    process.env.LLM_FLAG = 'true';
+    expect(shouldUsePrompts()).toBe(true);
+  });
 });
--- packages/llm/src/redisWorker.ts
+++ packages/llm/src/redisWorker.ts
@@ -1,14 +1,18 @@
 import { createClient } from 'redis';
 import * as dotenv from 'dotenv';
 import { sendPrompts } from './model';
-import ollama from "ollama";
+import ollama from 'ollama';
 
 // Load environment variables from .env file
 dotenv.config();
 
 let redis;
 
-if (process.env.REDIS_HOST && process.env.REDIS_PORT && process.env.REDIS_PASSWORD) {
+if (
+  process.env.REDIS_HOST &&
+  process.env.REDIS_PORT &&
+  process.env.REDIS_PASSWORD
+) {
   redis = createClient({
     socket: {
       host: process.env.REDIS_HOST,
@@ -17,7 +21,7 @@ if (process.env.REDIS_HOST && process.env.REDIS_PORT && process.env.REDIS_PASSWO
     password: process.env.REDIS_PASSWORD
   });
 } else {
-  throw "Redis variables missing from llm package .env";
+  throw 'Redis variables missing from llm package .env';
 }
 
 async function processJobs() {
@@ -33,8 +37,8 @@ async function processJobs() {
       const jobResponse = await redis.brPop(jobQueue, 0); // Wait indefinitely for a job
 
       if (jobResponse) {
-        console.log("Job Found")
-        console.log(jobResponse.element)
+        console.log('Job Found');
+        console.log(jobResponse.element);
         const element = jobResponse.element; // Access the element property directly
         const job = JSON.parse(element);
         const { jobID, jobData, responseQueue } = job;
@@ -59,9 +63,9 @@ async function processJobs() {
 }
 
 async function preloadModel() {
-  console.log("Preloading model...");
-  await ollama.pull({ model: "deepseek-llm:7b" });
-  console.log("Model ready to use.");
+  console.log('Preloading model...');
+  await ollama.pull({ model: 'deepseek-llm:7b' });
+  console.log('Model ready to use.');
 }
 
 preloadModel();
