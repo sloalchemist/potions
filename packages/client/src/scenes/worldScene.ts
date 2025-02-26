219d4f433e6094e50e89a5c090ec662d0c7ceb08
KyleQ1 <kylequinlan09@gmail.com>
2025-02-26 15:01:14 -0800

    Make wasd movement update with server better
    Before the wasd movement was wildly out of sync with server. Now updating it more periodically to make it better.

--- packages/client/src/scenes/worldScene.ts
+++ packages/client/src/scenes/worldScene.ts
@@ -59,6 +59,7 @@ export class WorldScene extends Phaser.Scene {
     d: false
   };
   lastKeyUp = '';
+  lastPublishTime: number = 0;
 
   constructor() {
     super({ key: 'WorldScene' });
@@ -495,7 +496,6 @@ export class WorldScene extends Phaser.Scene {
     this.hero = sprite;
   }
 
-  count = 0;
   update() {
     if (gameState !== 'stateInitialized') {
       this.hideWorld();
@@ -543,24 +543,13 @@ export class WorldScene extends Phaser.Scene {
       );
     }
 
-    if (this.count > 50) {
-      this.count = 0;
-      this.handlePlayerMovement(true);
-    } else {
-      this.count++;
-      this.handlePlayerMovement(false);
-    }
-  }
-
-  keyChange() {
-    let different = false;
-    for (const key in this.keys) {
-      if (this.keys[key] !== this.prevKeys[key]) {
-        different = true;
-        break;
-      }
-    }
-    return different;
+    const now = Date.now();
+    let publish = false;
+    if (now - this.lastPublishTime >= 100) {
+      publish = true;
+      this.lastPublishTime = now;
+    } 
+    this.handlePlayerMovement(publish)
   }
 
   handlePlayerMovement(publish: boolean) {
@@ -580,7 +569,6 @@ export class WorldScene extends Phaser.Scene {
     let moveX = player.position.x;
     let moveY = player.position.y;
 
-    let moved = false;
     let newX = moveX;
     let newY = moveY;
 
@@ -599,13 +587,13 @@ export class WorldScene extends Phaser.Scene {
 
     // Check if the next step is blocked
     const nextItem = world.getItemAt(newX, newY);
-    if (!!nextItem && !nextItem.isWalkable(player.unlocks)) {
+    if (nextItem && !nextItem.isWalkable(player.unlocks)) {
       return;
     }
 
-    // Check if your position has changed
-    moved = newX !== moveX || newY !== moveY;
-    if (!moved) return;
+    // If no movement, return
+    if (newX === moveX && newY === moveY)
+      return;
 
     let roundedX;
     let roundedY;
@@ -623,7 +611,6 @@ export class WorldScene extends Phaser.Scene {
     // NOTE: the code in the 'else' block moves the player on the client side
     //       publishPlayerPosition() calls that code itself, so player will
     //       move on the client side for whichever case
-
     if (publish) {
       this.prevKeys = { ...this.keys };
       publishPlayerPosition(target);
@@ -632,9 +619,6 @@ export class WorldScene extends Phaser.Scene {
       const path = world.generatePath(player.unlocks, player.position!, target);
       player.path = path;
     }
-
-    const movementKeys = ['a', 'w', 's', 'd'];
-    movementKeys.forEach((k) => (this.keys[k] = false));
   }
 
   showGameOver() {
