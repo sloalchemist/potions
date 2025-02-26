56da9d59637a8fa2a2afdde18becc9625cbb58af
Tyler Pham <137601404+TylerNP@users.noreply.github.com>
2025-02-26 14:39:17 -0800

    chore: refactored push metrics to be optional

--- packages/server/src/grafana/dataLogger.ts
+++ packages/server/src/grafana/dataLogger.ts
@@ -9,15 +9,16 @@ import {
   Gauge,
   Pushgateway
 } from 'prom-client';
-import { createServer } from 'http';
 import 'dotenv/config';
 import { getEnv } from '@rt-potion/common';
 import { worldID } from '../services/setup';
+import { requestClock } from './request';
 
 export class DataLogger {
   private static register = new Registry();
 
-  static tick_log = 0;
+  static tick_log: number[] = [];
+  static timeSinceTick = Date.now();
 
   private static mobGauge = new Gauge({
     name: 'num_mobs',
@@ -46,7 +47,7 @@ export class DataLogger {
 
   private static tickLatency = new Gauge({
     name: 'tick_latency',
-    help: 'Average number of latency of ticks per 10 seconds'
+    help: 'p90 of latency between ticks'
   });
 
   static {
@@ -61,14 +62,24 @@ export class DataLogger {
 
   static logTick() {
     this.tickCounter.inc(1);
-    DataLogger.tick_log++;
+
+    // Store backlog of tick latency
+    const now = Date.now();
+    DataLogger.tick_log.push(now - DataLogger.timeSinceTick);
+    DataLogger.timeSinceTick = now;
   }
 
   static logData() {
     const num_mobs = Mob.getAllMobIDs().length as number;
     const num_items = Item.getAllItemIDs().length as number;
-    const tick_time = 10000 / DataLogger.tick_log;
-    DataLogger.tick_log = 0;
+    DataLogger.tick_log.sort();
+    let tick_time = 0;
+    if (DataLogger.tick_log.length !== 0) {
+      // Get p90 for latency
+      tick_time =
+        DataLogger.tick_log[Math.ceil((DataLogger.tick_log.length - 1) * 0.9)];
+    }
+    DataLogger.tick_log = [];
     const tick_id = DB.prepare(
       `
             SELECT tick FROM ticks;
@@ -92,54 +103,45 @@ export class DataLogger {
   static getMetrics() {
     return this.register.metrics();
   }
-
-  static startMetricsServer(port: number = 3030) {
-    createServer(async (req, res) => {
-      if (req.url === '/metrics') {
-        res.writeHead(200, { 'Content-Type': this.register.contentType });
-        res.end(await this.getMetrics());
-      } else {
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-      }
-    }).listen(port, () => {
-      console.log(
-        `Prometheus metrics available at http://localhost:${port}/metrics`
-      );
-    });
-  }
 }
 
 // Setup Connection To Pushgateway
-export class DataPusher {
-  private static gateway = new Pushgateway(getEnv('METRIC_URL'), {
+function pushMetrics() {
+  let gatewayURL: string;
+  try {
+    gatewayURL = getEnv('METRIC_URL');
+  } catch {
+    console.log('ENV for pushgateway server not set disabling pushing metrics');
+    return;
+  }
+
+  const gateway = new Pushgateway(gatewayURL, {
     timeout: 5000, //Set the request timeout to 5000ms
     agent: new Agent({
       keepAlive: true
     })
   });
 
-  private static lastPushTime = Date.now();
+  // Load metrics under World Name 'Metric'
+  gateway
+    .pushAdd({ jobName: `${worldID} Metrics` })
+    .then()
+    .catch((e) => console.log(e));
 
-  // Load metrics under 'Metric'
-  static {
-    DataPusher.gateway
-      .pushAdd({ jobName: `${worldID} Metrics` })
-      .then()
-      .catch((e) => console.log(e));
-  }
-
-  static pushData() {
-    DataPusher.gateway
+  function pushData() {
+    gateway
       .push({ jobName: `${worldID} Metrics` })
       .then()
       .catch((e) => console.log(e));
   }
 
-  static pusherTimer() {
-    //const now = Date.now();
-    //const deltaTime = now - this.lastPushTime;
+  function pusherTimer() {
     DataLogger.logData();
-    DataPusher.pushData();
-    this.lastPushTime = Date.now();
+    pushData();
   }
+
+  setInterval(pusherTimer, 10000);
 }
+
+pushMetrics();
+requestClock();
