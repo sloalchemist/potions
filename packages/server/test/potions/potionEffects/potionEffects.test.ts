56521950916627a78cb566559049bd9030db7ded
Mcray3000 <69219980+Mcray3000@users.noreply.github.com>
2025-02-25 19:22:31 -0800

    feat: added test for player drinking tar

--- packages/server/test/potions/potionEffects/potionEffects.test.ts
+++ packages/server/test/potions/potionEffects/potionEffects.test.ts
@@ -770,6 +770,47 @@ describe('Try to consume purple potion in various cases', () => {
   });
 });
 
+// TOXIC POTION TEST
+
+describe('Test consumption of toxic potions', () => {
+  test('Test Drinking Tar', () => {
+    const position: Coord = { x: 0, y: 0 };
+    const potionLocation: Coord = { x: 1, y: 0 };
+
+    // create a player
+    mobFactory.makeMob('player', position, 'TestID', 'TestPlayer');
+    const testMob = Mob.getMob('TestID');
+    expect(testMob).not.toBeNull();
+
+    // create a tar potion
+    itemGenerator.createItem({
+      type: 'potion',
+      subtype: String(hexStringToNumber('#1B1212')),
+      position: potionLocation,
+      carriedBy: testMob
+    });
+    const potion = Item.getItemIDAt(potionLocation);
+    expect(potion).not.toBeNull();
+    const potionItem = Item.getItem(potion!);
+    expect(potionItem).not.toBeNull();
+
+    // ensure the player is carrying the potion
+    expect(testMob!.carrying).not.toBeNull();
+    expect(testMob!.carrying!.type).toBe('potion');
+    expect(testMob!.carrying!.subtype).toBe(
+      String(hexStringToNumber('#1B1212'))
+    );
+
+    // have the player drink the tar
+    const testDrink = new Drink();
+    const test = testDrink.interact(testMob!, potionItem!);
+    expect(test).toBe(true);
+
+    // Player should be dead
+    expect(testMob!.health).toBe(0);
+  });
+});
+
 afterAll(() => {
   DB.close();
 });
