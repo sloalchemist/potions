b581e982f92491bc0bd5c4da373c7a23dd635fcf
Tyler Pham <137601404+TylerNP@users.noreply.github.com>
2025-02-27 10:53:36 -0800

    Prometheus 318 (#536)

--- packages/server/src/services/setup.ts
+++ packages/server/src/services/setup.ts
@@ -13,7 +13,6 @@ import {
   uploadLocalData
 } from './supabaseStorage';
 import { shouldUploadDB } from '../util/dataUploadUtil';
-import { DataLogger } from '../grafana/dataLogger';
 import { getEnv } from '@rt-potion/common';
 
 let lastUpdateTime = Date.now();
@@ -81,8 +80,6 @@ async function initializeAsync() {
 }
 
 initializeAsync();
-
-DataLogger.startMetricsServer();
 
 // Used for update on developer cheat
 export function setLastUploadTime(time: number) {
