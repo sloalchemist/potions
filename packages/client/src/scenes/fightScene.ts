e303ee718dc66424db4a6f364fa1aab383ecc3f0
NickPerlich <64299263+NickPerlich@users.noreply.github.com>
2025-02-26 16:19:22 -0800

    fight screen shows up

--- packages/client/src/scenes/fightScene.ts
+++ packages/client/src/scenes/fightScene.ts
@@ -3,61 +3,26 @@ import { SCREEN_HEIGHT, SCREEN_WIDTH } from '../config';
 
 export class FightScene extends Phaser.Scene {
   private messageStack!: MessageStack; 
-  private brewColor = 0x000000;
-  private numIngredients = 0;
 
   constructor() {
     super({ key: 'FightScene' });
   }
 
   preload() {
-    this.load.image('brewImage', 'static/potion-brewing.png');
+    this.load.image('fightScreenImage', 'static/fightScreen.png');
   }
 
   create() {
-    const barBackground = this.add.graphics();
-    barBackground.fillStyle(0x9eb9d4, 1);
-    barBackground.fillRect(
-      180,
-      290,
-      this.game.scale.width * 0.4,
-      this.game.scale.height * -0.3
-    );
-
-    const barBackgroundColored = this.add.graphics();
-    barBackgroundColored.fillStyle(this.brewColor, 1);
-    barBackgroundColored.fillRect(
-      180,
-      290,
-      this.game.scale.width * 0.4,
-      this.game.scale.height * -0.1 * this.numIngredients
-    );
-
-    const circleBackground = this.add.graphics();
-    circleBackground.fillStyle(this.brewColor, 1);
-    circleBackground.fillRect(
-      90,
-      90,
-      this.game.scale.width * 0.4,
-      this.game.scale.height * 0.3
-    );
-
-    const overlayImage = this.add.image(
+    // Add the fight screen image 
+    const background = this.add.image(
       SCREEN_WIDTH / 2,
       SCREEN_HEIGHT / 2,
-      'brewImage'
+      'fightScreenImage'
     ); // Adjust x, y and texture as needed
-    overlayImage.setScrollFactor(0); // Prevent image from moving when scenes scroll
-    console.log('BrewScene created');
+    background.setScrollFactor(0); // Prevent image from moving when scenes scroll
+    console.log('FightScene created');
 
     this.messageStack = new MessageStack(this);
   }
 
-  setBrewColor(color: number) {
-    this.brewColor = color;
-  }
-
-  setNumIngredients(num: number) {
-    this.numIngredients = num;
-  }
 }
