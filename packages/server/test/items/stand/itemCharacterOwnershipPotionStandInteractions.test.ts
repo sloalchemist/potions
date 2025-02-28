2510ffd38cde71936cb7b516e0b91a54dc9a2bdc
nonetypes <alralsto@calpoly.edu>
2025-02-27 19:14:07 -0800

    test: add test for server-side individual ownership

---
+++ packages/server/test/items/stand/itemCharacterOwnershipPotionStandInteractions.test.ts
@@ -0,0 +1,67 @@
+import { commonSetup, world, itemGenerator } from '../../testSetup';
+import { DB } from '../../../src/services/database';
+import { mobFactory } from '../../../src/mobs/mobFactory';
+import { Community } from '../../../src/community/community';
+import { Item } from '../../../src/items/item';
+import { CollectGold } from '../../../src/items/uses/stand/collectGold';
+import { Mob } from '../../../src/mobs/mob';
+import { Coord } from '@rt-potion/common';
+
+beforeEach(() => {
+  commonSetup();
+  Community.makeVillage('alchemists', 'Alchemists guild');
+  mobFactory.loadTemplates(world.mobTypes);
+});
+
+describe('Potion Stand Ownership Tests', () => {
+  test('Owner can collect gold, non-owner cannot', () => {
+    const ownerID: string = 'ownerID';
+    const standPosition: Coord = { x: 0, y: 1 };
+    const playerPosition: Coord = { x: 0, y: 0 };
+
+    // Create Owner character
+    mobFactory.makeMob('player', playerPosition, ownerID, 'owner');
+    const owner = Mob.getMob(ownerID);
+    expect(owner).toBeDefined();
+    expect(owner!.gold).toBe(0);
+
+    // Create Non-Owner character
+    mobFactory.makeMob('player', playerPosition, 'notOwnerID', 'nonOwner');
+    const nonOwner = Mob.getMob('notOwnerID');
+    expect(nonOwner).toBeDefined();
+    expect(nonOwner!.gold).toBe(0);
+
+    // Create potion stand owned by OwnerCharacter
+    itemGenerator.createItem({
+      type: 'potion-stand',
+      subtype: '255',
+      position: standPosition,
+      ownedByCharacter: ownerID,
+      attributes: {
+        templateType: 'potion',
+        items: 0,
+        gold: 50,
+        capacity: 20
+      }
+    });
+    const standID = Item.getItemIDAt(standPosition);
+    expect(standID).not.toBeNull();
+    const potionStand = Item.getItem(standID!);
+    expect(potionStand).toBeDefined();
+
+    // Attempt gold collection by non-owner
+    const collectGold = new CollectGold();
+    const nonOwnerResult = collectGold.interact(nonOwner!, potionStand!);
+    expect(nonOwnerResult).toBe(false);
+    expect(nonOwner!.gold).toBe(0);
+
+    // Attempt gold collection by owner
+    const ownerResult = collectGold.interact(owner!, potionStand!);
+    expect(ownerResult).toBe(true);
+    expect(owner!.gold).toBe(50);
+  });
+});
+
+afterEach(() => {
+  DB.close();
+});
