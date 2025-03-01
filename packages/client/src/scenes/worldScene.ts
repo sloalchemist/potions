0c0f9a144eca959dfd046432bb12543da316164a
Adrian Hernandez <hernandeza257@gmail.com>
2025-02-28 19:55:08 -0800

    fixed linting issues

--- packages/client/src/scenes/worldScene.ts
+++ packages/client/src/scenes/worldScene.ts
@@ -85,7 +85,6 @@ export class WorldScene extends Phaser.Scene {
     this.load.json(
       'global_data',
       'https://potions.gg/world_assets/global.json'
-
     );
     this.load.json(
       'world_specific_data',
