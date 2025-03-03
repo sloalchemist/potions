00c7301838d076d41d4b5ea85176ef6e4fedd012
Charlotte Maples <cgmaples13@gmail.com>
2025-03-03 13:47:47 -0800

    feat: added tests for red potions

---
+++ packages/server/test/potions/potionEffects/redPotion.test.ts
@@ -0,0 +1,115 @@
+import { commonSetup, world, itemGenerator } from '../../testSetup';
+import { DB } from '../../../src/services/database';
+import { mobFactory } from '../../../src/mobs/mobFactory';
+import { Community } from '../../../src/community/community';
+import { Item } from '../../../src/items/item';
+import { Mob } from '../../../src/mobs/mob';
+import { Drink } from '../../../src/items/uses/drink';
+import { FantasyDate } from '../../../src/date/fantasyDate';
+import { Coord } from '@rt-potion/common';
+import { hexStringToNumber } from '../../../src/util/colorUtil';
+
+beforeEach(() => {
+  commonSetup();
+  Community.makeVillage('alchemists', 'Alchemists guild');
+  Community.makeVillage('blobs', 'Blobby town');
+  mobFactory.loadTemplates(world.mobTypes);
+});
+
+describe('Try to consume red potion in various cases', () => {
+    test('Test red potion consumption back to back', () => {
+      FantasyDate.initialDate();
+      const position: Coord = { x: 0, y: 0 };
+      const potionLocation: Coord = { x: 1, y: 0 };
+  
+      // create a player
+      mobFactory.makeMob('player', position, 'TestID', 'TestPlayer');
+      const testMob = Mob.getMob('TestID');
+      expect(testMob).not.toBeNull();
+
+      // update max health
+      testMob!.changeMaxHealth(100);
+      expect(testMob!._maxHealth).toBe(200);
+  
+      // create a potion
+      itemGenerator.createItem({
+        type: 'potion',
+        subtype: String(hexStringToNumber('#ff0000')),
+        position: potionLocation,
+        carriedBy: testMob
+      });
+      const potion = Item.getItemIDAt(potionLocation);
+      expect(potion).not.toBeNull();
+      const potionItem = Item.getItem(potion!);
+      expect(potionItem).not.toBeNull();
+  
+      // ensure the player is carrying the potion
+      expect(testMob!.carrying).not.toBeNull();
+      expect(testMob!.carrying!.type).toBe('potion');
+      expect(testMob!.carrying!.subtype).toBe(
+        String(hexStringToNumber('#ff0000'))
+      );
+  
+      // set initial health
+      const startHealth = testMob!.health;
+      expect(startHealth).toBe(100);
+  
+      // have the player drink the potion
+      const testDrink = new Drink();
+      const test = testDrink.interact(testMob!, potionItem!);
+      expect(test).toBe(true);
+  
+      for (let i = 0; i < 15; i++) {
+        // 15 ticks to check stacking
+        FantasyDate.runTick();
+      }
+      testMob?.tick(500);
+  
+      // check to make sure potion is not being carried
+      expect(testMob!.carrying).toBeUndefined();
+  
+      // get new health from DB
+      const health_1red = DB.prepare(
+        `
+              SELECT health FROM mobView WHERE id = :id
+          `
+      ).get({ id: testMob!.id }) as { health: number };
+  
+      // check attributes on player
+      expect(health_1red.health).toBe(150);
+  
+      // create a potion
+      itemGenerator.createItem({
+        type: 'potion',
+        subtype: String(hexStringToNumber('#ff0000')),
+        position: potionLocation,
+        carriedBy: testMob
+      }); 
+      const potion2 = Item.getItemIDAt(potionLocation);
+      expect(potion2).not.toBeNull();
+      const potionItem2 = Item.getItem(potion2!);
+      expect(potionItem2).not.toBeNull();
+  
+      // ensure the player is carrying the potion
+      expect(testMob!.carrying).not.toBeNull();
+      expect(testMob!.carrying!.type).toBe('potion');
+      expect(testMob!.carrying!.subtype).toBe(
+        String(hexStringToNumber('#ff0000'))
+      );
+  
+      // have the player drink the potion
+      const testDrink2 = new Drink();
+      const test2 = testDrink2.interact(testMob!, potionItem2!);
+      expect(test2).toBe(true);
+  
+      // check to make sure potion is not being carried
+      expect(testMob!.carrying).toBeUndefined();
+  
+      expect(testMob!.health).toBe(200);
+    });
+  });
+  
+afterAll(() => {
+  DB.close();
+});
+  
