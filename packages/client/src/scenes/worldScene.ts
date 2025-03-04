c3ee9ea2cf964a560c630134fc451d1ebe8eb43b
0felicia0 <fpatel2903@gmail.com>
2025-03-03 20:02:28 -0800

    Made shipwreck transparent when character walks behind

--- packages/client/src/scenes/worldScene.ts
+++ packages/client/src/scenes/worldScene.ts
@@ -29,6 +29,7 @@ import {
 } from '../utils/developerCheats';
 import { buttonStyle, nameButtonHoverStyle } from './loadWorldScene';
 import { Item } from '../world/item';
+import { SpriteItem } from '../sprite/sprite_item';
 
 export let world: World;
 let needsAnimationsLoaded: boolean = true;
@@ -534,6 +535,30 @@ export class WorldScene extends Phaser.Scene {
       Object.values(world.houses).forEach((house) => {
         const spriteHouse = house as SpriteHouse;
         spriteHouse.animate(Math.floor(x), Math.floor(y));
+      });
+
+      Object.values(world.items).forEach((shipwreck) => {
+        const ship = shipwreck as SpriteItem;
+        const heroX = Math.floor(x);
+        const heroY = Math.floor(y);
+
+        // Calculate the width and height in tiles
+        const shipWidthTiles = 110/32;
+        const shipHeightTiles = 115/32;
+
+        if (
+          ship.itemType.type === 'shipwreck' &&
+          (
+            heroX > 27 - shipWidthTiles/2 - 1 &&
+            heroX <= 27 + shipWidthTiles/2 + 1 &&
+            heroY > 28 - shipHeightTiles/2 &&
+            heroY <= 28 + shipHeightTiles/2 
+          )
+        ) {
+          ship.sprite.setAlpha(0.5);
+        } else {
+          ship.sprite.setAlpha(1);
+        }
       });
     }
 
