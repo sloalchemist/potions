5f92c6bb03e8dc0122d44f708d90fee25c7b2f24
Parshana007 <parshana007@gmail.com>
2025-03-05 13:26:13 -0800

    Created new branch w/ volcano as transparent

--- packages/client/src/scenes/worldScene.ts
+++ packages/client/src/scenes/worldScene.ts
@@ -29,6 +29,7 @@ import {
 } from '../utils/developerCheats';
 import { buttonStyle, nameButtonHoverStyle } from './loadWorldScene';
 import { Item } from '../world/item';
+import { SpriteItem } from '../sprite/sprite_item';
 
 export let world: World;
 let needsAnimationsLoaded: boolean = true;
@@ -540,6 +541,27 @@ export class WorldScene extends Phaser.Scene {
       Object.values(world.houses).forEach((house) => {
         const spriteHouse = house as SpriteHouse;
         spriteHouse.animate(Math.floor(x), Math.floor(y));
+      });
+      Object.values(world.items).forEach((volcano) => {
+        const vol = volcano as SpriteItem;
+        const heroX = Math.floor(x);
+        const heroY = Math.floor(y);
+
+        // Calculate the width and height in tiles
+        const volWidthTiles = 238 / TILE_SIZE;
+        const volHeightTiles = 204 / TILE_SIZE;
+
+        if (
+          vol.itemType.type === 'volcano' &&
+          heroX > 16 - volWidthTiles / 2 - 1 &&
+          heroX <= 16 + volWidthTiles / 2 + 1 &&
+          heroY > 24 - volHeightTiles / 2 &&
+          heroY <= 24 + volHeightTiles / 2
+        ) {
+          vol.sprite.setAlpha(0.5);
+        } else {
+          vol.sprite.setAlpha(1);
+        }
       });
     }
 
