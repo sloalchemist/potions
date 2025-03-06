cf3fdc09c0e33b235eda09a073bd0caadb4645e2
Adrian Hernandez <hernandeza257@gmail.com>
2025-03-05 21:14:42 -0800

    please fix linting issues

--- packages/json-generator/test/index.test.ts
+++ packages/json-generator/test/index.test.ts
@@ -13,7 +13,6 @@ describe('validate global json', () => {
       path.join(__dirname, '../../client/world_assets/global.json'),
       'utf8'
     );
-
     expect(JSON.parse(actual)).toStrictEqual(JSON.parse(expected));
   });
 
@@ -23,7 +22,6 @@ describe('validate global json', () => {
       path.join(__dirname, '../../server/world_assets/global.json'),
       'utf8'
     );
-
     expect(JSON.parse(actual)).toStrictEqual(JSON.parse(expected));
   });
 });
