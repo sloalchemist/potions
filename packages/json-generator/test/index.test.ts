4e0f5c6029ca05683e8a537810169a1d209ed4c5
Adam Mikacich <adammikacich@gmail.com>
2025-03-11 20:27:50 -0700

    fix: mock the writeFileSync function

--- packages/json-generator/test/index.test.ts
+++ packages/json-generator/test/index.test.ts
@@ -1,11 +1,26 @@
-import { readFileSync } from 'fs';
 import path from 'node:path';
 import { globalJsonSchema } from '../src/schema';
 import { executeWithArgs } from '../src/generator';
 
+// Mock only the writeFileSync function of the fs module
+jest.mock('fs', () => {
+  const originalModule = jest.requireActual('fs');
+  return {
+    ...originalModule,
+    writeFileSync: jest.fn()
+  };
+});
+
+// Import fs after mocking
+import * as fs from 'fs';
+
 describe('generator processes global.json', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('global.json passes validation', () => {
-    const globalJsonString = readFileSync(
+    const globalJsonString = fs.readFileSync(
       path.join(__dirname, '../global.json'),
       'utf8'
     );
@@ -16,19 +31,23 @@ describe('generator processes global.json', () => {
   });
 
   it('client global.json passes validation', () => {
+    // Get the mocked writeFileSync function
+    const writeFileSyncMock = fs.writeFileSync as jest.Mock;
+
     executeWithArgs(['client']);
 
-    const clientGlobalJsonString = readFileSync(
-      path.join(__dirname, '../../client/world_assets/global.json'),
-      'utf8'
-    );
+    expect(writeFileSyncMock).toHaveBeenCalled();
+
+    const clientGlobalJsonString = writeFileSyncMock.mock.calls[0][1] as string;
     const clientGlobalJsonObject = JSON.parse(clientGlobalJsonString);
 
-    // expect there to be exactly 3 keys
+    // Expect there to be exactly 3 keys in the client global.json
     expect(Object.keys(clientGlobalJsonObject).sort()).toEqual([
       'item_types',
       'mob_types',
       'portals'
     ]);
+
+    writeFileSyncMock.mockClear();
   });
 });
