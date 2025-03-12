cc40fc119d529eeb4383652e895ba8dc86edee97
Adam Mikacich <adammikacich@gmail.com>
2025-03-11 20:09:53 -0700

    fix: add validation test for client global.json

--- packages/json-generator/test/index.test.ts
+++ packages/json-generator/test/index.test.ts
@@ -1,6 +1,7 @@
 import { readFileSync } from 'fs';
 import path from 'node:path';
 import { globalJsonSchema } from '../src/schema';
+import { executeWithArgs } from '../src/generator';
 
 describe('generator processes global.json', () => {
   it('global.json passes validation', () => {
@@ -10,7 +11,24 @@ describe('generator processes global.json', () => {
     );
     const globalJsonObject = JSON.parse(globalJsonString);
 
-    const result = globalJsonSchema.safeParse(globalJsonObject);
-    expect(result.error).toBeUndefined();
+    const result = globalJsonSchema.parse(globalJsonObject);
+    expect(result).toBeDefined();
+  });
+
+  it('client global.json passes validation', () => {
+    executeWithArgs(['client']);
+
+    const clientGlobalJsonString = readFileSync(
+      path.join(__dirname, '../../client/world_assets/global.json'),
+      'utf8'
+    );
+    const clientGlobalJsonObject = JSON.parse(clientGlobalJsonString);
+
+    // expect there to be exactly 3 keys
+    expect(Object.keys(clientGlobalJsonObject).sort()).toEqual([
+      'item_types',
+      'mob_types',
+      'portals'
+    ]);
   });
 });
