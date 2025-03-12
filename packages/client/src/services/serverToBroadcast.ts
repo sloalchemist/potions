c9ea0ef086bb9b5a5604fd340f8b7e5055cd0b95
ethanhandelman <ehandelm@calpoly.edu>
2025-03-12 16:13:17 -0700

    fix: location

--- packages/client/src/services/serverToBroadcast.ts
+++ packages/client/src/services/serverToBroadcast.ts
@@ -109,7 +109,7 @@ export function setupBroadcast(
     const mob = world.mobs[data.id] as SpriteMob;
     if (mob == undefined) {
       console.warn(
-        `client/src/serverToBroadcast.ts: Ably attempting to assign 'doing' to undefined mob with id ${data.id}`
+        `client/src/services/serverToBroadcast.ts: Ably attempting to assign 'doing' to undefined mob with id ${data.id}`
       );
     } else {
       mob.doing = data.action;
